9d023cf64debee130dd714138c4621d8678a0613
Mathieu Alorent <github@kumy.net>
2018-04-02 21:28:29 +0200

    Fix codacy issues for JS

--- funkcje.js
+++ funkcje.js
@@ -2,26 +2,28 @@ function zliczaj(maxlen) {
   var licznik = document.getElementById("licznik");
   var pole = document.getElementById("poledoliczenia");
   var tekst = pole.value; // the entire text of the textarea field
-  var dl_tresc = pole.value.length; // long text from a textarea
+  var dlTresc = pole.value.length; // long text from a textarea
   var prawdziwa = 0; // character count
   var entery = 0;
 
-  for (i = 0; i < dl_tresc; i++) {
+  for (i = 0; i < dlTresc; i++) {
     if (tekst.charAt(i) == "\n") {
       prawdziwa++;
       entery++;
-      if (navigator.appName != "Netscape")
+      if (navigator.appName !== "Netscape") {
         i++;
       }
+    }
     prawdziwa++;
   } // end for i
 
   licznik.value = maxlen - prawdziwa; // display message on the number of characters
   if (prawdziwa > maxlen) {
-    if (navigator.appName != "Netscape")
+    if (navigator.appName !== "Netscape") {
       pole.value = pole.substring(0, maxlen);
-    else
+    } else {
       pole.value = pole.value.substring(0, maxlen - entery);
+    }
     licznik.value = 0;
     alert("max length " + maxlen + "!");
   }
@@ -49,13 +51,12 @@ function copy_value_to_innerHTML(in_id, out_id) {
 // aktywuje odpowiednie pola raksa ;)
 
 function RuchyPola(typ) {
-  var doc = document;
-  if (typ == 0 || typ == 3 || typ == 5) {
+  if (typ === 0 || typ === 3 || typ === 5) {
     $('#latlon').prop('disabled', false);
     $('#wpt').prop('disabled', false);
     $('#btn_sprawdzskrzynke').prop('disabled', false);
     $('#NazwaSkrzynki').prop('disabled', false);
-  } else if (typ == 1 || typ == 2 || typ == 4 || typ == 6) {
+  } else if (typ === 1 || typ === 2 || typ === 4 || typ === 6) {
     $('#latlon').prop('disabled', true);
     $('#wpt').prop('disabled', true);
     $('#btn_sprawdzskrzynke').prop('disabled', true);
@@ -69,105 +70,105 @@ function sprawdzWpt(e) {
     var KeyID = (window.event)
       ? event.keyCode
       : e.keyCode;
-    if (KeyID == 9 || (KeyID >= 16 && KeyID <= 20) || (KeyID >= 33 && KeyID <= 40))
+    if (KeyID === 9 || (KeyID >= 16 && KeyID <= 20) || (KeyID >= 33 && KeyID <= 40)) {
       return;
     }
+  }
 
   var doc = document;
-  var pole_in_id = 'wpt';
-  var pole_out_id = 'wynikWpt';
+  var poleInId = 'wpt';
+  var poleOutId = 'wynikWpt';
 
-  var pole_latlon = doc.getElementById('latlon');
-  var pole_NazwaSkrzynk = doc.getElementById('NazwaSkrzynki');
-  var pole_ajax_status = doc.getElementById('ajax_status');
+  var poleLatlon = doc.getElementById('latlon');
+  var poleNazwaSkrzynk = doc.getElementById('NazwaSkrzynki');
+  var poleAjaxStatus = doc.getElementById('ajax_status');
 
-  var pole_in_value = (doc.getElementById(pole_in_id).value).replace(/^\s\s*/, '').replace(/\s\s*$/, '');
-  var pole_out = doc.getElementById(pole_out_id);
+  var poleInValue = (doc.getElementById(poleInId).value).replace(/^\s\s*/, '').replace(/\s\s*$/, '');
+  var poleOut = doc.getElementById(poleOutId);
 
-  if (pole_in_value.length >= 6) {
-    if (e || (pole_ajax_status.title != pole_in_value)) {
-      pole_ajax_status.innerHTML = 'Retrieving...';
-      pole_ajax_status.title = pole_in_value;
+  if (poleInValue.length >= 6) {
+    if (e || (poleAjaxStatus.title !== poleInValue)) {
+      poleAjaxStatus.innerHTML = 'Retrieving...';
+      poleAjaxStatus.title = poleInValue;
       $.get('szukaj-ajax.php', {
         'skad': 'ajax',
-        'wpt': pole_in_value
+        'wpt': poleInValue
       }, function(data) {
-        pole_ajax_status.innerHTML = '';
+        poleAjaxStatus.innerHTML = '';
         var json = JSON.parse(data);
-        if (json.lat != '') {
-          pole_latlon.value = json.lat + " " + json.lon;
-          pole_latlon.disabled = false;
-          //pole_latlon.disabled=true;
-          pole_NazwaSkrzynk.disabled = true;
-          pole_out.innerHTML = json.tresc;
+        if (json.lat !== '') {
+          poleLatlon.value = json.lat + " " + json.lon;
+          poleLatlon.disabled = false;
+          //poleLatlon.disabled=true;
+          poleNazwaSkrzynk.disabled = true;
+          poleOut.innerHTML = json.tresc;
         } else {
-          pole_latlon.disabled = false;
-          pole_latlon.value = '';
+          poleLatlon.disabled = false;
+          poleLatlon.value = '';
           //tmp.focus();
-          pole_NazwaSkrzynk.disabled = true;
-          pole_out.innerHTML = json.tresc;
+          poleNazwaSkrzynk.disabled = true;
+          poleOut.innerHTML = json.tresc;
         }
       });
     }
   } else {
-    pole_latlon.disabled = false;
-    pole_latlon.value = '';
+    poleLatlon.disabled = false;
+    poleLatlon.value = '';
 
-    pole_NazwaSkrzynk.disabled = false;
-    pole_NazwaSkrzynk.value = '';
+    poleNazwaSkrzynk.disabled = false;
+    poleNazwaSkrzynk.value = '';
 
-    pole_out.innerHTML = '';
-    pole_ajax_status.title = '';
+    poleOut.innerHTML = '';
+    poleAjaxStatus.title = '';
   }
 }
 
 function sprawdzNazwe() {
   var doc = document;
-  var pole_in_id = "NazwaSkrzynki";
-  var pole_out_id = 'wynikWpt';
+  var poleInId = "NazwaSkrzynki";
+  var poleOutId = 'wynikWpt';
 
-  var pole_in_value = (doc.getElementById(pole_in_id).value).replace(/^\s\s*/, '').replace(/\s\s*$/, '');;
+  var poleInValue = (doc.getElementById(poleInId).value).replace(/^\s\s*/, '').replace(/\s\s*$/, '');
 
-  var pole_wpt = doc.getElementById('wpt');
-  var pole_latlon = doc.getElementById('latlon');
-  var pole_NazwaSkrzynk = doc.getElementById('NazwaSkrzynki');
-  var pole_ajax_status = doc.getElementById('ajax_status');
+  var poleWpt = doc.getElementById('wpt');
+  var poleLatlon = doc.getElementById('latlon');
+  var poleAjaxStatus = doc.getElementById('ajax_status');
 
-  var pole_out = doc.getElementById(pole_out_id);
+  var poleOut = doc.getElementById(poleOutId);
 
-  if (pole_in_value.length >= 5) {
-    pole_out.innerHTML = '';
-    pole_ajax_status.innerHTML = 'Retrieving...';
-    pole_ajax_status.title = pole_in_value;
+  if (poleInValue.length >= 5) {
+    poleOut.innerHTML = '';
+    poleAjaxStatus.innerHTML = 'Retrieving...';
+    poleAjaxStatus.title = poleInValue;
     $.get('szukaj-ajax.php', {
       'skad': 'ajax',
-      'NazwaSkrzynki': pole_in_value
+      'NazwaSkrzynki': poleInValue
     }, function(data) {
-      pole_ajax_status.innerHTML = '';
+      poleAjaxStatus.innerHTML = '';
       var json = JSON.parse(data);
-      if (json.IleSkrzynek == 1) {
-        pole_latlon.disabled = true;
-        pole_latlon.value = json.lat + ' ' + json.lon;
-        pole_wpt.value = json.wpt;
-        pole_out.innerHTML = json.tresc;
+      if (json.IleSkrzynek === 1) {
+        poleLatlon.disabled = true;
+        poleLatlon.value = json.lat + ' ' + json.lon;
+        poleWpt.value = json.wpt;
+        poleOut.innerHTML = json.tresc;
       } else {
-        pole_latlon.disabled = false;
-        pole_latlon.value = '';
-        pole_wpt.disabled = false;
-        pole_wpt.value = '';
-        pole_out.innerHTML = json.tresc;
+        poleLatlon.disabled = false;
+        poleLatlon.value = '';
+        poleWpt.disabled = false;
+        poleWpt.value = '';
+        poleOut.innerHTML = json.tresc;
       }
     });
   } else {
-    pole_wpt.disabled = false;
-    pole_wpt.value = '';
+    poleWpt.disabled = false;
+    poleWpt.value = '';
 
-    pole_latlon.disabled = false;
-    pole_latlon.value = '';
+    poleLatlon.disabled = false;
+    poleLatlon.value = '';
 
-    pole_out.innerHTML = '';
-    pole_ajax_status.title = '';
-    pole_ajax_status.innerHTML = 'Enter at least 5 characters';
+    poleOut.innerHTML = '';
+    poleAjaxStatus.title = '';
+    poleAjaxStatus.innerHTML = 'Enter at least 5 characters';
   }
 
 }
@@ -178,40 +179,41 @@ function sprawdzGK(e) {
     var KeyID = (window.event)
       ? event.keyCode
       : e.keyCode;
-    if (KeyID == 9 || (KeyID >= 16 && KeyID <= 20) || (KeyID >= 33 && KeyID <= 40))
+    if (KeyID === 9 || (KeyID >= 16 && KeyID <= 20) || (KeyID >= 33 && KeyID <= 40)) {
       return;
     }
 
-  var pole_in_value = (document.getElementById('nr').value).replace(/^\s\s*/, '').replace(/\s\s*$/, '');
-  var pole_out = document.getElementById('wynikNr');
+    var poleInValue = (document.getElementById('nr').value).replace(/^\s\s*/, '').replace(/\s\s*$/, '');
+    var poleOut = document.getElementById('wynikNr');
 
-  //if(pole_in_value.length == 6)
-  if (/^[a-zA-Z0-9]{6}(\.[a-zA-Z0-9]{6})*$/.test(pole_in_value)) {
-    pole_out.innerHTML = 'Retrieving...';
-    $.get('szukaj-ajax.php', {
-      'skad': 'ajax',
-      'nr': pole_in_value
-    }, function(data) {
-      pole_out.innerHTML = data;
-    });
-  } else {
-    pole_out.innerHTML = '';
+    //if(poleInValue.length == 6)
+    if (/^[a-zA-Z0-9]{6}(\.[a-zA-Z0-9]{6})*$/.test(poleInValue)) {
+      poleOut.innerHTML = 'Retrieving...';
+      $.get('szukaj-ajax.php', {
+        'skad': 'ajax',
+        'nr': poleInValue
+      }, function(data) {
+        poleOut.innerHTML = data;
+      });
+    } else {
+      poleOut.innerHTML = '';
+    }
   }
 }
 
 function CzySkasowac(theLink, tresc) {
-  var is_confirmed = confirm('Are you sure you want to delete ' + tresc);
-  if (is_confirmed) {
+  var isConfirmed = confirm('Are you sure you want to delete ' + tresc);
+  if (isConfirmed) {
     theLink.href += '&confirmed=1';
   }
 
-  return is_confirmed;
+  return isConfirmed;
 }
 
 function Potwierdz(theLink, tresc) {
-  var is_confirmed = confirm(tresc);
-  if (is_confirmed) {
+  var isConfirmed = confirm(tresc);
+  if (isConfirmed) {
     theLink.href += '?confirmed=1';
   }
-  return is_confirmed;
+  return isConfirmed;
 }
