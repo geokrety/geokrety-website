de4c6c5fc37cfa5b662a4e61d13c67a400d0198c
Mathieu Alorent <github@kumy.net>
2018-04-06 00:29:58 +0200

    Automatically export new strings

--- scripts/export-translations-github.sh
+++ scripts/export-translations-github.sh
@@ -6,7 +6,7 @@ set -e
 
 GIT_BRANCH=master
 if [ "$TRAVIS_PULL_REQUEST" == "false" -a "$TRAVIS_BRANCH" == "$GIT_BRANCH" ]; then
-  echo -e "Starting translation import\n"
+  echo -e "Starting translation export\n"
   CROWDIN_CLI_URL=https://downloads.crowdin.com/cli/v2/crowdin-cli.zip
   CROWDIN_CLI_VERSION=2.0.22
 
@@ -23,18 +23,21 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" -a "$TRAVIS_BRANCH" == "$GIT_BRANCH" ]; t
   # Get latest strings
   cd geokrety-crowdin
   wget ${CROWDIN_CLI_URL} -O crowdin-cli.zip; unzip crowdin-cli.zip; rm crowdin-cli.zip
-  java -jar $CROWDIN_CLI_VERSION/crowdin-cli.jar download
-  rm -fr $CROWDIN_CLI_VERSION
 
-  # Build mo files
-  for file in $(ls -d rzeczy/lang/*.UTF-8); do msgfmt ${file}/LC_MESSAGES/messages.po -o ${file}/LC_MESSAGES/messages.mo; done
+  # Extract new strings
+  xgettext --from-code=UTF-8 -o $TMP/messages.pot --language=PHP -f rzeczy/lang/input-files-for-xgettext
+  tsmarty2c -o $TMP/smarty.pot ./templates/index.html ./templates/krety-m.html  ./templates/krety.html ./templates/krety_not_logged_in.html ./templates/krety_logged_in.html
+  msgcat -o rzeczy/messages.po.txt $TMP/messages.pot $TMP/smarty.pot
+
+  # Upload new strings
+  java -jar $CROWDIN_CLI_VERSION/crowdin-cli.jar upload
 
   #add, commit and push files
-  git add -f rzeczy/lang/
-  git commit -m "Automatic translation import (build $TRAVIS_BUILD_NUMBER) [ci skip]"
+  git add -f rzeczy/messages.po.txt
+  git commit -m "Automatic in-context translation import (build $TRAVIS_BUILD_NUMBER) [ci skip]"
   git push origin $GIT_BRANCH || true
 
   cd /tmp/
   rm -fr $TMP
-  echo -e "Done importing new transaltions\n"
+  echo -e "Done uploading new strings\n"
 fi
