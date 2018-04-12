f1949880278bb1846474f570868d80d6ea264b02
Mathieu Alorent <github@kumy.net>
2018-04-12 23:19:46 +0200

    Language synchronization

--- scripts/import-translations-github.sh
+++ scripts/import-translations-github.sh
@@ -24,15 +24,17 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" -a "$TRAVIS_BRANCH" == "$GIT_BRANCH" ]; t
   # Get latest strings
   cd geokrety-crowdin
   wget ${CROWDIN_CLI_URL} -O crowdin-cli.zip; unzip crowdin-cli.zip; rm crowdin-cli.zip
-  java -jar $CROWDIN_CLI_VERSION/crowdin-cli.jar download
+  java -jar $CROWDIN_CLI_VERSION/crowdin-cli.jar download --ignore-match
   rm -fr $CROWDIN_CLI_VERSION
 
   # Build mo files
-  for file in $(ls -d rzeczy/lang/*.UTF-8); do msgfmt ${file}/LC_MESSAGES/messages.po -o ${file}/LC_MESSAGES/messages.mo; done
+  for file in $(ls -d rzeczy/lang/*.UTF-8); do
+    msgfmt -v ${file}/LC_MESSAGES/messages.po -o ${file}/LC_MESSAGES/messages.mo;
+  done
 
   #add, commit and push files
-  if [ -n "$(git status --porcelain)" ]; then
-    git add -f rzeczy/lang/*.UTF-8
+  git add rzeczy/lang/*.UTF-8
+  if ! git diff --cached --exit-code; then
     git commit -m "Automatic translation import (build $TRAVIS_BUILD_NUMBER) [ci skip]"
     git push origin $GIT_BRANCH
     echo -e "Done importing new transaltions\n"
