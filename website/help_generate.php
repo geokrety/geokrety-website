a66a442049281e82347212a890637232463b237f
Mathieu <github@kumy.net>
2018-06-10 19:27:55 +0200

    Move en help page to a single html file
    This will permit to upload help page to crowdin.

--- website/help_generate.php
+++ website/help_generate.php
@@ -9,8 +9,12 @@ $smarty_cache_this_page = 0; // this page should be cached for n seconds
 require_once 'smarty_start.php';
 $TYTUL = _('Help');
 
+$HEAD .= '<style type="text/css">.logtypes TD{padding:5px; vertical-align: middle; border-bottom: 1px solid #ccc}
+.logtypes .mid TD{text-align: middle;}
+</style>';
+
 $jezyk = strtolower($_GET['help']); // w jamim języku ma być help?
-if (!in_array($jezyk, ['cz', 'de', 'fr', 'hu', 'pl', 'ru', 'sk'])) {
+if (!in_array($jezyk, ['en', 'cz', 'de', 'fr', 'hu', 'pl', 'ru', 'sk'])) {
     die;
 }
 
