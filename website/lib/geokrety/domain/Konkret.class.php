a3d42d3921dfdc44a87fb1d479c9a52c4b9f2676
Boly38 <boly38@gmail.com>
2018-10-26 20:38:43 +0200

    Improve LdJson generation
    - following #7 initial LdJson SEO feature
    - increase ld-json version to 0.0.14 following torann/json-ld#42
    - use new fields from 0.0.14 (publisher, lang, date)
    - add konkret dedicated ldjson helper and test
    - use full url for 'sameAs' field, that fixes #121
    - manual tests with google tool https://search.google.com/structured-data/testing-tool/u/0/?hl=fr
    - fix phpunit tests
    - improve irc notifications details
    - fix codacy

---
+++ website/lib/geokrety/domain/Konkret.class.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Geokrety\Domain;
+
+class Konkret {
+    public $name;
+    public $description;
+    public $url;
+    public $author;
+    public $authorUrl;
+    public $datePublished;
+    public $imageUrl;
+    public $keywords;
+    public $ratingCount;
+    public $ratingAvg;
+    public $konkretLogs;
+}
