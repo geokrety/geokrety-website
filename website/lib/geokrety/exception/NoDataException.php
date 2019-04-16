96661d3800acf868d095dac89eba1dcc4ccfbc6c
Boly38 <boly38@gmail.com>
2019-04-16 13:43:20 +0200

    Fixes #283 remove NoDataException onTripUpdate
    - return empty array in case of no trip
    - phpcs

--- website/lib/geokrety/exception/NoDataException.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Geokrety\Exception;
-
-class NoDataException extends \Exception {
-    public function __construct($message = 'no data found') {
-        parent::__construct($message);
-    }
-}
