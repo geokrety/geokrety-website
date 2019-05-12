2669d33acc98808024744e66a54f1ab9ae019ebf
Mathieu <github@kumy.net>
2019-05-12 18:33:47 +0200

    Factirize pictureRepo

--- website/lib/geokrety/repository/PictureRepository.class.php
+++ website/lib/geokrety/repository/PictureRepository.class.php
@@ -4,12 +4,6 @@ namespace Geokrety\Repository;
 
 class PictureRepository extends AbstractRepository {
 
-    const SELECT_PICTURE = <<<EOQUERY
-SELECT    ob.obrazekid, ob.id, ob.typ as type, ob.id_kreta as gk_id, ob.user as user_id,
-          ob.plik as filename, ob.opis as legend
-FROM      `gk-obrazki` ob
-EOQUERY;
-
     const SELECT_PICTURES_DETAILS = <<<EOQUERY
 SELECT    ob.obrazekid, ob.id, ob.typ as type, ob.id_kreta as gk_id, ob.user as user_id,
           ob.plik as filename, ob.opis as legend, gk.nazwa as gk_name,
@@ -26,176 +20,48 @@ EOQUERY;
         $avatarType = 2;
         // $avatarType = \Geokrety\Domain\AVATAR; // TODO why this doesn't work???
         $where = <<<EOQUERY
-  WHERE user = ?
-  AND   typ = $avatarType
-  LIMIT 1
+  WHERE     user = ?
+  AND       typ = $avatarType
+  LIMIT     1
 EOQUERY;
 
-        $sql = self::SELECT_PICTURE.$where;
-        if ($this->verbose) {
-            echo "\n$sql\n";
+        $sql = self::SELECT_PICTURES_DETAILS.$where;
+
+        $pictures = $this->getPicturesSql($sql, array($id));
+        if (sizeof($pictures) === 1) {
+            return $pictures[0];
         }
-
-        if (!($stmt = $this->dblink->prepare($sql))) {
-            throw new \Exception($action.' prepare failed: ('.$this->dblink->errno.') '.$this->dblink->error);
-        }
-        if (!$stmt->bind_param('d', $id)) {
-            throw new \Exception($action.' binding parameters failed: ('.$stmt->errno.') '.$stmt->error);
-        }
-
-        if (!$stmt->execute()) {
-            throw new \Exception($action.' execute failed: ('.$stmt->errno.') '.$stmt->error);
-        }
-
-        $stmt->store_result();
-        $nbRow = $stmt->num_rows;
-
-        if ($nbRow == 0) {
-            return null;
-        }
-
-        // associate result vars
-        $stmt->bind_result($id, $tripId, $type, $geokretId, $userId, $filename, $legend);
-
-        $picture = new \Geokrety\Domain\PictureUser();
-        while ($stmt->fetch()) {
-            $picture->id = $id;
-            $picture->tripId = $tripId;
-            $picture->type = $type;
-            $picture->geokretId = $geokretId;
-            $picture->userId = $userId;
-            $picture->filename = $filename;
-            $picture->legend = $legend;
-        }
-
-        $stmt->close();
-
-        return $picture;
+        return null;
     }
 
     public function getAvatarByGeokretId($id) {
         $id = $this->validationService->ensureIntGTE('id', $id, 1);
 
         $where = <<<EOQUERY
-  WHERE obrazekid = ?
-  LIMIT 1
+  WHERE     obrazekid = ?
+  LIMIT     1
 EOQUERY;
 
-        $sql = self::SELECT_PICTURE.$where;
-        if ($this->verbose) {
-            echo "\n$sql\n";
+        $sql = self::SELECT_PICTURES_DETAILS.$where;
+
+        $pictures = $this->getPicturesSql($sql, array($id));
+        if (sizeof($pictures) === 1) {
+            return $pictures[0];
         }
-
-        if (!($stmt = $this->dblink->prepare($sql))) {
-            throw new \Exception($action.' prepare failed: ('.$this->dblink->errno.') '.$this->dblink->error);
-        }
-        if (!$stmt->bind_param('d', $id)) {
-            throw new \Exception($action.' binding parameters failed: ('.$stmt->errno.') '.$stmt->error);
-        }
-
-        if (!$stmt->execute()) {
-            throw new \Exception($action.' execute failed: ('.$stmt->errno.') '.$stmt->error);
-        }
-
-        $stmt->store_result();
-        $nbRow = $stmt->num_rows;
-
-        if ($nbRow == 0) {
-            return null;
-        }
-
-        // associate result vars
-        $stmt->bind_result($id, $tripId, $type, $geokretId, $userId, $filename, $legend);
-
-        $picture = new \Geokrety\Domain\PictureGeoKret();
-        while ($stmt->fetch()) {
-            $picture->id = $id;
-            $picture->tripId = $tripId;
-            $picture->type = $type;
-            $picture->geokretId = $geokretId;
-            $picture->userId = $userId;
-            $picture->filename = $filename;
-            $picture->legend = $legend;
-        }
-
-        $stmt->close();
-
-        return $picture;
+        return null;
     }
 
     public function getByGeokretId($id) {
         $id = $this->validationService->ensureIntGTE('id', $id, 1);
 
         $where = <<<EOQUERY
-  WHERE id_kreta = ?
-  ORDER BY obrazekid DESC
+  WHERE     id_kreta = ?
+  ORDER     BY obrazekid DESC
 EOQUERY;
 
-        $sql = self::SELECT_PICTURE.$where;
-        if ($this->verbose) {
-            echo "\n$sql\n";
-        }
+        $sql = self::SELECT_PICTURES_DETAILS.$where;
 
-        if (!($stmt = $this->dblink->prepare($sql))) {
-            throw new \Exception($action.' prepare failed: ('.$this->dblink->errno.') '.$this->dblink->error);
-        }
-        if (!$stmt->bind_param('d', $id)) {
-            throw new \Exception($action.' binding parameters failed: ('.$stmt->errno.') '.$stmt->error);
-        }
-
-        if (!$stmt->execute()) {
-            throw new \Exception($action.' execute failed: ('.$stmt->errno.') '.$stmt->error);
-        }
-
-        $stmt->store_result();
-        $nbRow = $stmt->num_rows;
-
-        if ($nbRow == 0) {
-            return array();
-        }
-
-        // associate result vars
-        $stmt->bind_result($id, $tripId, $type, $geokretId, $userId, $filename, $legend);
-
-        $pictures = array();
-        while ($stmt->fetch()) {
-            $picture = null;
-            switch ($type) {
-                case 0:
-                  $picture = new \Geokrety\Domain\PictureGeoKret();
-                  $picture->geokretId = $geokretId;
-                  $picture->geokretName = $geokretName;
-                  $picture->country = $country;
-                  $picture->date = $date;
-                  break;
-                case 1:
-                  $picture = new \Geokrety\Domain\PictureTrip();
-                  $picture->geokretId = $geokretId;
-                  $picture->geokretName = $geokretName;
-                  $picture->country = $country;
-                  $picture->date = $date;
-                  $picture->tripId = $tripId;
-                  break;
-                case 2:
-                  $picture = new \Geokrety\Domain\PictureUser();
-                  $picture->username = $username;
-                  break;
-                default:
-                  throw new \Exception("picture id:$id has a wrong type ($type) : ($stmt->errno) ".$stmt->error);
-            }
-            $picture->id = $id;
-            $picture->tripId = $tripId;
-            $picture->type = $type;
-            $picture->userId = $userId;
-            $picture->filename = $filename;
-            $picture->legend = $legend;
-
-            array_push($pictures, $picture);
-        }
-
-        $stmt->close();
-
-        return $pictures;
+        return $this->getPicturesSql($sql, array($id));
     }
 
     public function getById($id) {
