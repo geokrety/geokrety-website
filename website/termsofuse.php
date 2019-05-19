6c65d95367a292c214c8e181dd1e50041f3e1443
Mathieu <github@kumy.net>
2019-05-19 18:46:44 +0200

    Implement press corner

--- website/termsofuse.php
+++ website/termsofuse.php
@@ -5,9 +5,7 @@ require_once '__sentry.php';
 $smarty_cache_this_page = 3800; // this page should be cached for n seconds
 require_once 'smarty_start.php';
 
-$smarty->assign('content_template', 'terms_of_use.tpl');
-
 $TYTUL = _('Terms of use');
-
+$smarty->assign('content_template', 'terms_of_use.tpl');
 
 require_once 'smarty.php';
