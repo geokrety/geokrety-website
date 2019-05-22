75d2b1d2c8fa8f8e519e90f40f2c7142be892a64
Mathieu <github@kumy.net>
2019-05-22 18:55:48 +0200

    Fix #340: allow read the map for unauthenticated users

--- website/gkmap.php
+++ website/gkmap.php
@@ -4,7 +4,6 @@ require_once '__sentry.php';
 
 $smarty_cache_this_page = 0; // this page should be cached for n seconds
 require_once 'smarty_start.php';
-loginFirst();
 
 $smarty->assign('content_template', 'gkmap.tpl');
 $smarty->append('javascript', CDN_LEAFLET_JS);
