23675de0885ecfcf1a165beb561d149610cad412
kumy <kumy@users.noreply.github.com>
2019-07-21 10:12:55 +0200

    Fix collation + add comments

--- docker/mariadb/02_add_waypoint_sync.sql
+++ docker/mariadb/02_add_waypoint_sync.sql
@@ -1,7 +1,7 @@
 ALTER TABLE `gk-waypointy`
   ALTER `kraj` DROP DEFAULT;
 ALTER TABLE `gk-waypointy`
-  CHANGE COLUMN `kraj` `kraj` VARCHAR(200) NULL COLLATE 'utf8_polish_ci' AFTER `typ`;
+  CHANGE COLUMN `kraj` `kraj` VARCHAR(200) NULL COLLATE 'utf8mb4_unicode_ci' COMMENT 'full English country name' AFTER `typ`;
 
 ALTER TABLE `gk-waypointy`
   ALTER `lat` DROP DEFAULT,
@@ -11,12 +11,13 @@ ALTER TABLE `gk-waypointy`
   ALTER `typ` DROP DEFAULT,
   ALTER `link` DROP DEFAULT;
 ALTER TABLE `gk-waypointy`
+  CHANGE `country` `country` char(3) COLLATE 'utf8mb4_unicode_ci' NULL COMMENT 'country code as ISO 3166-1 alpha-2' AFTER `alt`,
   CHANGE COLUMN `lat` `lat` DOUBLE(8,5) NULL AFTER `waypoint`,
   CHANGE COLUMN `lon` `lon` DOUBLE(8,5) NULL AFTER `lat`,
-  CHANGE COLUMN `name` `name` VARCHAR(255) NULL COLLATE 'utf8_polish_ci' AFTER `country`,
-  CHANGE COLUMN `owner` `owner` VARCHAR(150) NULL COLLATE 'utf8_polish_ci' AFTER `name`,
-  CHANGE COLUMN `typ` `typ` VARCHAR(200) NULL COLLATE 'utf8_polish_ci' AFTER `owner`,
-  CHANGE COLUMN `link` `link` VARCHAR(255) NULL COLLATE 'utf8_polish_ci' AFTER `kraj`;
+  CHANGE COLUMN `name` `name` VARCHAR(255) NULL COLLATE 'utf8mb4_unicode_ci' AFTER `country`,
+  CHANGE COLUMN `owner` `owner` VARCHAR(150) NULL COLLATE 'utf8mb4_unicode_ci' AFTER `name`,
+  CHANGE COLUMN `typ` `typ` VARCHAR(200) NULL COLLATE 'utf8mb4_unicode_ci' AFTER `owner`,
+  CHANGE COLUMN `link` `link` VARCHAR(255) NULL COLLATE 'utf8mb4_unicode_ci' AFTER `kraj`;
 
 
 CREATE TABLE `gk-waypointy-sync` (
