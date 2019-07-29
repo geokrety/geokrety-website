59432532519268da907de5504b09ddeb5f315f5f
Mathieu <github@kumy.net>
2019-07-29 18:04:11 +0200

    Fix typo in German label instruction
    Thanks LeDompteur

--- website/lib/labels/Template.php
+++ website/lib/labels/Template.php
@@ -4,7 +4,7 @@ abstract class Template {
     private $manual_strings = array(
 'pl' => 'Instrukcja obsługi: 1. Zabierz geokreta. Zanotuj tracking code 2. Ukryj kreta w innym keszu. 3. Zarejestruj jego podróż na stronie https://geokrety.org',
 'en' => "User's manual: 1. Take this GeoKret. Please note down his Tracking Code. 2. Hide in another cache. 3. Register the trip at https://geokrety.org",
-'de' => 'Anleitung: 1. Nimm den GeoKret mit and notiere Dir den Tracking Code. 2. Verstecke ihn wieder in einem anderen Cache. 3. Logge seine Reise auf https://geokrety.org',
+'de' => 'Anleitung: 1. Nimm den GeoKret mit und notiere Dir den Tracking Code. 2. Verstecke ihn wieder in einem anderen Cache. 3. Logge seine Reise auf https://geokrety.org',
 'cz' => 'Navod pro uživatele: 1. Vem Geokrtka. Poznamenej si jeho Tracking Code. 2. Schovej ho v jiné kešce. 3. Registruj cestu na https://geokrety.org',
 'fr' => "Manuel de l'utilisateur: 1. Prenez ce GeoKret et notez son code de suivi. 2. Déposez le dans une autre cache. 3. Enregistrez son voyage sur https://geokrety.org",
 'ru' => 'Руководство пользователя: 1. Возьмите ГеоКрота. Запишите его Tracking Code. 2. Переместите его в другой тайник. 3. Сообщите об этом на https://geokrety.org',
