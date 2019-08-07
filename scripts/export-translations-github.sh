722d080f35ccb48e3d0ac0b04e026f61a96e556f
Mathieu <github@kumy.net>
2019-08-07 22:55:04 +0200

    Fix #395: Enable crowdin for new-theme branch

--- scripts/export-translations-github.sh
+++ scripts/export-translations-github.sh
@@ -4,8 +4,8 @@ set -e
 
 # https://gist.github.com/paolorotolo/404110603ba22ff0728b#file-import-translations-github-sh
 
-GIT_BRANCH=master
-if [ "$TRAVIS_PULL_REQUEST" == "false" -a "$TRAVIS_BRANCH" == "$GIT_BRANCH" ]; then
+CROWDIN_BRANCH=$(echo $TRAVIS_BRANCH | tr '/' '.')
+if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
   TMP=$(mktemp -d)
 
   #go to home and setup git
@@ -13,18 +13,19 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" -a "$TRAVIS_BRANCH" == "$GIT_BRANCH" ]; t
   git config --global user.email "geokrety@gmail.com"
   git config --global user.name "GeoKrety Bot"
 
-  git clone --branch=${GIT_BRANCH} https://$GITHUB_API_KEY@github.com/geokrety/geokrety-website.git geokrety-crowdin
+  git clone --branch=${TRAVIS_BRANCH} https://$GITHUB_API_KEY@github.com/geokrety/geokrety-website.git geokrety-crowdin
 
   # Get latest strings
   cd geokrety-crowdin
 
   # Extract new strings
-  xgettext --from-code=UTF-8 -o $TMP/messages.pot --language=PHP -f website/rzeczy/lang/input-files-for-xgettext
-  tsmarty2c -o $TMP/smarty.pot ./website/templates/smarty/ ./website/templates/index.html ./website/templates/krety-m.html ./website/templates/krety.html ./website/templates/krety_not_logged_in.html ./website/templates/krety_logged_in.html ./website/templates/waypointy-translations.html
+  find ./website/ -type f -name '*.php' -not -path './website/lib/*' -not -path './website/vendor/*' -not -path './website/templates/*' | sort > /tmp/input-files-for-xgettext
+  xgettext --from-code=UTF-8 -o $TMP/messages.pot --language=PHP -f /tmp/input-files-for-xgettext
+  tsmarty2c -o $TMP/smarty.pot ./website/templates/smarty ./website/templates/waypointy-translations.html
   msgcat -o website/rzeczy/messages.po.txt $TMP/messages.pot $TMP/smarty.pot
 
   # Upload new strings
-  crowdin upload --branch $GIT_BRANCH
+  crowdin upload --branch ${CROWDIN_BRANCH}
 
   #add, commit and push files
   read -r plus minus filename <<< $( git diff --numstat website/rzeczy/messages.po.txt )
@@ -33,7 +34,7 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" -a "$TRAVIS_BRANCH" == "$GIT_BRANCH" ]; t
   else
     git add -f website/rzeczy/messages.po.txt
     git commit -m "Automatic in-context translation import (build $TRAVIS_BUILD_NUMBER) [ci skip]"
-    git push origin $GIT_BRANCH || true
+    git push origin $TRAVIS_BRANCH || true
     echo -e "Done uploading new strings\n"
   fi
 fi
