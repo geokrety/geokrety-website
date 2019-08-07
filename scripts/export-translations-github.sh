b997d4c2d450ffa5f068245eb31eea8ab3ea0396
Mathieu <github@kumy.net>
2019-08-08 00:32:47 +0200

    Disable crowdin in context commit
    I'm really not sure if this was used in the in context process at
    all

--- scripts/export-translations-github.sh
+++ scripts/export-translations-github.sh
@@ -8,16 +8,6 @@ CROWDIN_BRANCH=$(echo $TRAVIS_BRANCH | tr '/' '.')
 if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
   TMP=$(mktemp -d)
 
-  #go to home and setup git
-  cd $TMP
-  git config --global user.email "geokrety@gmail.com"
-  git config --global user.name "GeoKrety Bot"
-
-  git clone --branch=${TRAVIS_BRANCH} https://$GITHUB_API_KEY@github.com/geokrety/geokrety-website.git geokrety-crowdin
-
-  # Get latest strings
-  cd geokrety-crowdin
-
   # Extract new strings
   find ./website/ -type f -name '*.php' -not -path './website/lib/*' -not -path './website/vendor/*' -not -path './website/templates/*' | sort > /tmp/input-files-for-xgettext
   xgettext --from-code=UTF-8 -o $TMP/messages.pot --language=PHP -f /tmp/input-files-for-xgettext
@@ -26,15 +16,4 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
 
   # Upload new strings
   crowdin upload --branch ${CROWDIN_BRANCH}
-
-  #add, commit and push files
-  read -r plus minus filename <<< $( git diff --numstat website/rzeczy/messages.po.txt )
-  if [ "$plus" = "$minus" -a "$plus" = "1" ]; then
-    echo "No new strings found."
-  else
-    git add -f website/rzeczy/messages.po.txt
-    git commit -m "Automatic in-context translation import (build $TRAVIS_BUILD_NUMBER) [ci skip]"
-    git push origin $TRAVIS_BRANCH || true
-    echo -e "Done uploading new strings\n"
-  fi
 fi
