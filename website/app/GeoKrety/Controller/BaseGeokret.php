6a942a491177cdd5f05c486e4fd1cc6a7b6c972e
Mathieu <github@kumy.net>
2019-08-28 23:26:21 +0200

    f3: allow find GeoKrety by decimal id too

--- website/app/GeoKrety/Controller/BaseGeokret.php
+++ website/app/GeoKrety/Controller/BaseGeokret.php
@@ -8,15 +8,14 @@ use GeoKrety\Model\Geokret;
 class BaseGeokret extends Base {
     public function beforeRoute($f3) {
         parent::beforeRoute($f3);
-        $gk = $f3->get('PARAMS.gkid');
-        if (strtoupper(substr($gk, 0, 2)) !== 'GK') {
-            Smarty::render('dialog/alert_404.tpl');
-            die();
+        $gkid = $f3->get('PARAMS.gkid');
+        if (strtoupper(substr($gkid, 0, 2)) === 'GK') {
+            $gkid = hexdec(substr($gkid, 2));
         }
 
         $geokret = new Geokret();
         $geokret->filter('owner_codes', array('user = ?', null));
-        $geokret->load(array('gkid = ?', hexdec(substr($gk, 2))));
+        $geokret->load(array('gkid = ?', $gkid));
         if ($geokret->dry()) {
             Smarty::render('dialog/alert_404.tpl');
             die();
