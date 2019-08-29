43f184697854a52c2d634f389c48424d1008106d
Mathieu <github@kumy.net>
2019-08-30 00:19:06 +0200

    f3: migrate contact-us / credits / hall of fame

--- website/app-templates/smarty/footer.tpl
+++ website/app-templates/smarty/footer.tpl
@@ -1,16 +1,16 @@
 <footer class="footer">
     <ul class="footer_bottom_ul">
       <li><a href="{'home'|alias}">{t}Home{/t}</a></li>
-      <li><a href="#">About</a></li>
+      <li><a href="{'help'|alias}#about">About</a></li>
       <li><a href="{'news_list'|alias}">News</a></li>
-      <li><a href="#">Contact</a></li>
+      <li><a href="{'contact_us'|alias}">Contact</a></li>
     </ul>
 
     <p class="text-center">
       {t escape=no url="https://github.com/geokrety/geokrety-website/blob/master/LICENSE"}Released under <a href="%1">MIT license</a>{/t}
     </p>
     <p class="text-center">
-      {t escape=no url="#"}Designed with love by <a href="%1">The GeoKrety Team</a>{/t}
+      {t escape=no url="{'hall_of_fame'|alias}"}Designed with love by <a href="%1">The GeoKrety Team</a>{/t}
     </p>
     <ul class="social_footer_ul text-center">
       <li><a href="https://www.facebook.com/groups/1624761011150615/about/">{fa icon="facebook"}</a></li>
---
+++ website/app/GeoKrety/Controller/HallOfFame.php
@@ -0,0 +1,148 @@
+<?php
+
+namespace GeoKrety\Controller;
+
+use GeoKrety\Service\Smarty;
+use GeoKrety\Model\User;
+
+class HallOfFame extends Base {
+    public function get($f3) {
+        $contributors_ids = array(1, 6262, 35313, 26422, 1789, 497, 30144, 196, 7861, 17135);
+        $contributors = array();
+
+        foreach ($contributors_ids as $userid) {
+            $user = new User();
+            $user->load(array('id = ?', $userid));
+            if ($user->valid()) {
+                $contributors[$user->username] = $user;
+            }
+        }
+        Smarty::assign('contributors', $contributors);
+
+        $credits = array();
+        // $credits[] = [
+        //     'name' => _('name'),
+        //     'icon' => _('logo'),
+        //     'icon_style' => 'background: rgba(34, 47, 69, 1);',
+        //     'icon_width' => '130px',
+        //     'link' => _('link'),
+        //     'desc' => _('decription'),
+        // ];
+        $credits[] = [
+            'name' => 'php',
+            'icon' => GK_CDN_LOGOS_URL.'/php-power-white.gif',
+            'link' => 'https://secure.php.net/',
+            'desc' => 'server-side scripting language',
+        ];
+        $credits[] = [
+            'name' => 'php extras',
+            'desc' => '<a href="https://getcomposer.org/">composer</a>, <a href="https://www.smarty.net/docsv2/fr/">smarty</a>, <a href="https://phpunit.de/">phpunit</a>, <a href="https://github.com/FriendsOfPHP/PHP-CS-Fixer">FriendsOfPHP/PHP-CS-Fixer</a>, <a href="https://github.com/Torann/json-ld">Torann/json-ld</a>',
+        ];
+        $credits[] = [
+            'name' => 'apache',
+            'icon' => GK_CDN_LOGOS_URL.'/Apache_Server_2.jpg',
+            'link' => 'https://httpd.apache.org/',
+            'desc' => 'web server application (and extensions)',
+        ];
+        $credits[] = [
+            'name' => 'mariadb',
+            'icon' => GK_CDN_LOGOS_URL.'/MariaDB-Foundation-vertical-small.png',
+            'link' => 'https://mariadb.org',
+            'desc' => 'relational database management system',
+        ];
+        $credits[] = [
+            'name' => 'docker',
+            'icon' => GK_CDN_LOGOS_URL.'/vertical.png',
+            'link' => 'https://www.docker.com',
+            'desc' => 'operating-system-level virtualization',
+        ];
+        $credits[] = [
+            'name' => 'git',
+            'icon' => GK_CDN_LOGOS_URL.'/2color-lightbg%402x.png',
+            'link' => 'https://git-scm.com',
+            'desc' => 'source code management',
+        ];
+        $credits[] = [
+            'name' => 'github',
+            'icon' => GK_CDN_LOGOS_URL.'/Octocat.png',
+            'link' => 'https://github.com',
+            'desc' => 'project management, hosting',
+        ];
+        $credits[] = [
+            'name' => 'travis',
+            'icon' => GK_CDN_LOGOS_URL.'/TravisCI-Mascot-pride.png',
+            'icon_width' => '70px',
+            'link' => 'https://docs.travis-ci.com/',
+            'desc' => 'continuous integration',
+        ];
+        $credits[] = [
+            'name' => 'codacy',
+            'icon' => GK_CDN_LOGOS_URL.'/logo-codacy.png',
+            'icon_style' => 'background: rgba(34, 47, 69, 1);',
+            'icon_width' => '130px',
+            'link' => 'https://opensource.codacy.com',
+            'desc' => 'manage code quality',
+        ];
+        $credits[] = [
+            'name' => 'crowdin',
+            'icon' => GK_CDN_LOGOS_URL.'/crowdin-TranslationManagementService-logo.png',
+            'icon_width' => '130px',
+            'link' => 'https://crowdin.com',
+            'desc' => 'manage translations',
+        ];
+        $credits[] = [
+            'name' => 'adnanh/webhook',
+            'icon' => GK_CDN_LOGOS_URL.'/logo-128x128.png',
+            'link' => 'https://github.com/adnanh/webhook',
+            'desc' => 'configurable incoming webhook server',
+        ];
+        $credits[] = [
+            'name' => 'Sentry',
+            'icon' => GK_CDN_LOGOS_URL.'/sentry-glyph-black.svg',
+            'link' => 'https://sentry.io/welcome/',
+            'desc' => 'Open-source error tracking',
+        ];
+        $credits[] = [
+            'name' => 'NASA SRTM',
+            'link' => 'https://www2.jpl.nasa.gov/srtm/',
+            'desc' => 'SRTM-30 to get the altitudes',
+        ];
+        $credits[] = [
+            'name' => 'Bootstrap',
+            'icon' => GK_CDN_LOGOS_URL.'/bootstrap-solid.svg',
+            'icon_width' => '50px',
+            'link' => 'https://getbootstrap.com/',
+            'desc' => 'HTML, CSS, and JS framework',
+        ];
+        $credits[] = [
+            'name' => 'flag-icon-css',
+            'icon' => GK_CDN_LOGOS_URL.'/flag-icon-css.jpg',
+            'link' => 'http://flag-icon-css.lip.is/',
+            'desc' => 'A collection of all country flags in SVG — plus the CSS',
+        ];
+        $credits[] = [
+            'name' => 'openstreetmap',
+            'icon' => GK_CDN_LOGOS_URL.'/Public-images-osm_logo.svg',
+            'icon_width' => '70px',
+            'link' => 'https://openstreetmap.org',
+            'desc' => 'Map',
+        ];
+        $credits[] = [
+            'name' => 'robot framework',
+            'icon' => GK_CDN_LOGOS_URL.'/robot-framework.png',
+            'icon_width' => '70px',
+            'link' => 'http://robotframework.org/',
+            'desc' => 'Automated tests (used by <a href="https://github.com/geokrety/geokrety-website-qa">geokrety-website-qa</a>)',
+        ];
+        $credits[] = [
+            'name' => 'reCAPTCHA',
+            'icon' => GK_CDN_LOGOS_URL.'/RecaptchaLogo.svg',
+            'icon_width' => '70px',
+            'link' => 'https://www.google.com/recaptcha/',
+            'desc' => 'Secure captcha (stop bots)',
+        ];
+        Smarty::assign('app_credits', $credits);
+
+        Smarty::render('pages/hall_of_fame.tpl');
+    }
+}
