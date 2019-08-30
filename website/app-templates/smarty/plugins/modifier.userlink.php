217bc47060a02df943540a3c29aadd0b4b667b73
Mathieu <github@kumy.net>
2019-08-30 14:13:51 +0200

    Resolve use of undefined variable

--- website/app-templates/smarty/plugins/modifier.userlink.php
+++ website/app-templates/smarty/plugins/modifier.userlink.php
@@ -12,15 +12,14 @@ require_once SMARTY_PLUGINS_DIR.'modifier.escape.php';
  * -------------------------------------------------------------
  */
 function smarty_modifier_userlink($user, ?string $alternative_name = null) {
-    $target_html = is_null($target) ? '' : ' target="'.$params['target'].'"';
-
     if (!$user->id) {
         $username = _('Anonymous');
         if (!is_null($alternative_name)) {
             $username = smarty_modifier_escape($alternative_name);
         }
+
         return '<em class="user-anonymous">'.$username.'</em>';
     }
 
-    return '<a href="'.\Base::instance()->alias('user_details', 'userid='.$user->id).'" title="'.sprintf('View %s\'s profile', smarty_modifier_escape($user->username)).'"'.$target_html.'>'.smarty_modifier_escape($user->username).'</a>';
+    return '<a href="'.\Base::instance()->alias('user_details', 'userid='.$user->id).'" title="'.sprintf('View %s\'s profile', smarty_modifier_escape($user->username)).'">'.smarty_modifier_escape($user->username).'</a>';
 }
