a888f88334aeb4036d686f1a45e33b05a252d0fb
Mathieu <github@kumy.net>
2019-08-30 12:23:39 +0200

    f3: migrate help/social_groups

--- website/app/GeoKrety/Controller/Help.php
+++ website/app/GeoKrety/Controller/Help.php
@@ -3,10 +3,51 @@
 namespace GeoKrety\Controller;
 
 use GeoKrety\Service\Smarty;
+use GeoKrety\Service\LanguageService;
 use GeoKrety\Controller\StaticPages;
 
 class Help extends Base {
     public function get($f3) {
+
+        $social_groups = array();
+        $social_groups[] = [
+            'link' => 'https://www.facebook.com/groups/1624761011150615/about/',
+            'service' => 'Facebook',
+            'title' => 'GeoKret en France',
+            'lang' => 'fr',
+        ];
+        $social_groups[] = [
+            'link' => 'https://www.facebook.com/groups/317897208552636/about/',
+            'service' => 'Facebook',
+            'title' => 'GeoCaching met GeoKrety.Org',
+            'lang' => 'nl',
+        ];
+        // Historical group
+        // $social_groups[] = [
+        //     'link' => 'https://plus.google.com/communities/100975961916549394786',
+        //     'service' => 'Google+',
+        //     'title' => 'GeoKrety Discussion',
+        //     'lang' => array('en', 'de', 'nl'),
+        // ];
+        $social_groups[] = [
+            'link' => 'https://forum.opencaching.pl/viewforum.php?f=11',
+            'service' => 'Opencaching Poland',
+            'title' => 'GeoKrety',
+            'lang' => 'pl',
+        ];
+        $social_groups[] = [
+            'link' => 'https://geoclub.de/forum/viewforum.php?f=102',
+            'service' => 'geoclub.de forum',
+            'title' => 'Geokrety',
+            'lang' => 'de',
+        ];
+        $social_groups[] = [
+            'link' => 'https://forum.opencaching.nl/viewforum.php?f=33',
+            'service' => 'Opencaching Benelux',
+            'title' => 'GeoKrety',
+            'lang' => 'nl',
+        ];
+        Smarty::assign('social_groups', $social_groups);
 
         foreach (explode(',', $f3->get('LANGUAGE')) as $lang) {
             $file = 'help-pages/'.$lang.'/help.html';
