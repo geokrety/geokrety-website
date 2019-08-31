3b4c6bcb2747d4c9eb19c4573fc52d421422205a
Mathieu <github@kumy.net>
2019-08-31 18:41:32 +0200

    crowdin: upload path according to new layout

--- scripts/export-translations-github.sh
+++ scripts/export-translations-github.sh
@@ -12,7 +12,7 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
   find ./website/ -type f -name '*.php' -not -path './website/old/*' -not -path './website/vendor/*' -not -path './website/templates/*' | sort > /tmp/input-files-for-xgettext
   xgettext --from-code=UTF-8 -o $TMP/messages.pot --language=PHP -f /tmp/input-files-for-xgettext
   tsmarty2c -o $TMP/smarty.pot ./website/app-templates
-  msgcat -o website/rzeczy/messages.po.txt $TMP/messages.pot $TMP/smarty.pot
+  msgcat -o website/app/languages/messages.po.txt $TMP/messages.pot $TMP/smarty.pot
 
   # Upload new strings
   crowdin upload --branch ${CROWDIN_BRANCH}
