ed6588158eb92621b0740c62ab2416a2cfdcd00b
Mathieu <github@kumy.net>
2019-09-01 13:43:31 +0200

    Compile smarty templates on container start

---
+++ website/app/GeoKrety/Controller/Cli/SmartyTemplates.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace GeoKrety\Controller\Cli;
+
+use GeoKrety\Service\Smarty;
+
+class SmartyTemplates {
+    public function clearCompiledTemplates() {
+        $smarty = Smarty::getSmarty();
+        $smarty->clearCompiledTemplate();
+        echo "\e[0;32mSmarty templates cleared\e[0m".PHP_EOL;
+    }
+
+    public function compileAllTemplates() {
+        $smarty = Smarty::getSmarty();
+        $smarty->compileAllTemplates('.tpl', true);
+        $smarty->compileAllTemplates('.tpl.js', true);
+        echo "\e[0;32mSmarty templates compiled\e[0m".PHP_EOL;
+
+        if (exec('chown -R www-data.www-data '.GK_SMARTY_COMPILE_DIR) === null) {
+            echo "\e[0;31mError changing templates owner to php user\e[0m".PHP_EOL;
+        }
+
+    }
+}
