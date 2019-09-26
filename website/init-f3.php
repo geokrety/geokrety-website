a6caac6d0b82ebf6270c289e7ca988920b06c451
Mathieu <github@kumy.net>
2019-09-26 18:58:03 +0200

    Relates #507: Display all php errors when GK_F3_DEBUG is enabled

--- website/init-f3.php
+++ website/init-f3.php
@@ -9,12 +9,18 @@ $f3->config('app/authorizations.ini');
 // Create GK_* consts from environments
 \GeoKrety\Service\Config::instance();
 
+// // Falsum
+// Falsum\Run::handler();
+
 \Sentry\init(['dsn' => GK_SENTRY_DSN, 'environment' => GK_SENTRY_ENV, 'release' => GK_APP_VERSION]);
 
 $f3->set('UI', GK_F3_UI);
 $f3->set('TMP', GK_F3_TMP);
 $f3->set('CACHE', GK_F3_CACHE);
 $f3->set('DEBUG', GK_F3_DEBUG);
+if (GK_F3_DEBUG) {
+    error_reporting(E_ALL);
+}
 
 if (!$f3->exists('DB')) {
     $f3->set('DB', new \DB\SQL(GK_DB_DSN, GK_DB_USER, GK_DB_PASSWORD, [\PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8mb4;']));
