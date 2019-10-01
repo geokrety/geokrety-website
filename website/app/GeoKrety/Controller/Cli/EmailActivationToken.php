6afaf2618df1dcaa727e6942f9526656de7063c8
Mathieu <github@kumy.net>
2019-10-01 22:57:42 +0200

    Add a cron system

---
+++ website/app/GeoKrety/Controller/Cli/EmailActivationToken.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace GeoKrety\Controller\Cli;
+
+use GeoKrety\Model\EmailActivation;
+
+class EmailActivationToken {
+    public function prune(\Base $f3) {
+        if ($f3->exists('LOCK.EmailActivationToken.prune')) {
+            echo "\e[0;31mAnother task is already running\e[0m".PHP_EOL;
+
+            return 1;
+        }
+
+        echo "\e[0;32mLaunch task\e[0m".PHP_EOL;
+        $f3->set('LOCK.EmailActivationToken.prune', 'true', 60);
+        EmailActivation::expireOldTokens();
+        $f3->clear('LOCK.EmailActivationToken.prune');
+    }
+}
