6bb5163e1410caa3cea95f88be359a536e27c60b
Mathieu <github@kumy.net>
2019-10-02 19:25:17 +0200

    Paginate results when regenerating all users statpics

--- website/app/GeoKrety/Controller/Cli/UserBanner.php
+++ website/app/GeoKrety/Controller/Cli/UserBanner.php
@@ -7,19 +7,30 @@ use GeoKrety\Model\User;
 class UserBanner {
     public function generateAll() {
         $userModel = new User();
-        $users = $userModel->find();
 
-        if (!$users) {
+        $user_count = $userModel->count();
+        if (!$user_count) {
             echo "\e[0;32mNo user found\e[0m".PHP_EOL;
 
             return;
         }
+        echo sprintf('%d users to proceed', $user_count).PHP_EOL;
 
-        foreach ($users as $user) {
-            \GeoKrety\Service\UserBanner::generate($user);
-            echo sprintf(' * generated banner for  %5s ; %s', $user->id, $user->username).PHP_EOL;
+        // Paginate the table resultset as it may blow ram!
+        define('PER_PAGE', 10);
+        $total_pages = ceil($user_count / PER_PAGE);
+        $counter = 0;
+        for ($i = 0; $i < $total_pages; ++$i) {
+            $subset = $userModel->paginate($i, PER_PAGE);
+            foreach ($subset['subset'] as $user) {
+                \GeoKrety\Service\UserBanner::generate($user);
+                echo sprintf(' * generated banner for  %5s ; %s', $user->id, $user->username).PHP_EOL;
+                ob_flush();
+                ++$counter;
+            }
         }
-        echo sprintf("\e[0;32mConverted %d users\e[0m", count($users)).PHP_EOL;
+
+        echo sprintf("\e[0;32mGenerated %d user banners\e[0m", $counter).PHP_EOL;
     }
 
     public function generateByUserId($f3) {
