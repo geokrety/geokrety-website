7414fb7376bc8f480869ddf3791cfa06e3f51e43
Mathieu <github@kumy.net>
2019-10-04 22:04:19 +0200

    Fix typo in cli output

--- website/app/GeoKrety/Controller/Cli/GetText.php
+++ website/app/GeoKrety/Controller/Cli/GetText.php
@@ -5,7 +5,7 @@ namespace GeoKrety\Controller\Cli;
 class GetText {
     public function buildTranslations() {
         if (exec('find ../app/languages/ -name \*.po -execdir msgfmt -v messages.po -o messages.mo \;') === null) {
-            echo "\e[0;31mError building .mo files to .po\e[0m".PHP_EOL;
+            echo "\e[0;31mError building .mo files from .po\e[0m".PHP_EOL;
         } else {
             echo "\e[0;32mTranslation files built successfully\e[0m".PHP_EOL;
             echo "\e[0;33;41mWarning: `php` must be restarted for the new files to be read again\e[0m".PHP_EOL;
