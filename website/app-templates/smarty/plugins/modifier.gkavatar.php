ffe8b9b5e84d2db5c5948dbe62d13e54b1dae13d
Mathieu <github@kumy.net>
2020-02-25 23:21:41 +0100

    Refactor: remove dead code

--- website/app-templates/smarty/plugins/modifier.gkavatar.php
+++ website/app-templates/smarty/plugins/modifier.gkavatar.php
@@ -14,12 +14,9 @@ function smarty_modifier_gkavatar(\GeoKrety\Model\Geokret $geokret) {
         return;
     }
 
-    $gkhex = $geokret->gkid;
     $iconUrl = GK_CDN_ICONS_URL.'/idcard.png';
     $alt = _('has avatar icon');
     $title = _('GeoKret has an avatar');
-    // $miniatureUrl = CONFIG_CDN_OBRAZKI_MALE.'/'.$gk->avatarFilename;
-    // $fullSizeUrl = CONFIG_CDN_OBRAZKI.'/'.$gk->avatarFilename;
 
     return <<< EOT
 <img src="$iconUrl" width="14" height="10" alt="$alt" title="$title" />
