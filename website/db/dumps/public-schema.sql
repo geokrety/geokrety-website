66f8c4e902c60ce3d69d3c087064a244c3812012
Mathieu <github@kumy.net>
2020-04-17 17:52:49 +0200

    Add missing sequence in public schema export

--- website/db/dumps/public-schema.sql
+++ website/db/dumps/public-schema.sql
@@ -5,7 +5,7 @@
 -- Dumped from database version 12.2 (Debian 12.2-2.pgdg100+1)
 -- Dumped by pg_dump version 12.2 (Ubuntu 12.2-2.pgdg19.10+1)
 
--- Started on 2020-04-15 19:00:57 CEST
+-- Started on 2020-04-17 17:51:40 CEST
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -18,17 +18,51 @@ SET xmloption = content;
 SET client_min_messages = warning;
 SET row_security = off;
 
+--
+-- TOC entry 9 (class 2615 OID 2200)
+-- Name: public; Type: SCHEMA; Schema: -; Owner: geokrety
+--
+
+CREATE SCHEMA public;
+
+
+ALTER SCHEMA public OWNER TO geokrety;
+
+--
+-- TOC entry 5622 (class 0 OID 0)
+-- Dependencies: 9
+-- Name: SCHEMA public; Type: COMMENT; Schema: -; Owner: geokrety
+--
+
+COMMENT ON SCHEMA public IS 'standard public schema';
+
+
+--
+-- TOC entry 281 (class 1259 OID 510504)
+-- Name: countries_id_seq; Type: SEQUENCE; Schema: public; Owner: geokrety
+--
+
+CREATE SEQUENCE public.countries_id_seq
+    START WITH 1
+    INCREMENT BY 246
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.countries_id_seq OWNER TO geokrety;
+
 SET default_tablespace = '';
 
 SET default_table_access_method = heap;
 
 --
--- TOC entry 281 (class 1259 OID 498649)
+-- TOC entry 279 (class 1259 OID 498649)
 -- Name: countries; Type: TABLE; Schema: public; Owner: geokrety
 --
 
 CREATE TABLE public.countries (
-    id integer DEFAULT nextval('public.gk_countries_id_seq'::regclass) NOT NULL,
+    id integer DEFAULT nextval('public.countries_id_seq'::regclass) NOT NULL,
     iso_a2 character varying(2) NOT NULL,
     geog public.geography NOT NULL
 );
@@ -37,7 +71,7 @@ CREATE TABLE public.countries (
 ALTER TABLE public.countries OWNER TO geokrety;
 
 --
--- TOC entry 225 (class 1259 OID 106059)
+-- TOC entry 223 (class 1259 OID 106059)
 -- Name: srtm; Type: TABLE; Schema: public; Owner: geokrety
 --
 
@@ -51,7 +85,7 @@ CREATE TABLE public.srtm (
 ALTER TABLE public.srtm OWNER TO geokrety;
 
 --
--- TOC entry 226 (class 1259 OID 106065)
+-- TOC entry 224 (class 1259 OID 106065)
 -- Name: srtm_metadata; Type: VIEW; Schema: public; Owner: geokrety
 --
 
@@ -75,7 +109,7 @@ CREATE VIEW public.srtm_metadata AS
 ALTER TABLE public.srtm_metadata OWNER TO geokrety;
 
 --
--- TOC entry 208 (class 1259 OID 104455)
+-- TOC entry 207 (class 1259 OID 104455)
 -- Name: srtm_rid_seq; Type: SEQUENCE; Schema: public; Owner: geokrety
 --
 
@@ -91,8 +125,8 @@ CREATE SEQUENCE public.srtm_rid_seq
 ALTER TABLE public.srtm_rid_seq OWNER TO geokrety;
 
 --
--- TOC entry 5594 (class 0 OID 0)
--- Dependencies: 208
+-- TOC entry 5623 (class 0 OID 0)
+-- Dependencies: 207
 -- Name: srtm_rid_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: geokrety
 --
 
@@ -100,7 +134,7 @@ ALTER SEQUENCE public.srtm_rid_seq OWNED BY public.srtm.rid;
 
 
 --
--- TOC entry 5444 (class 2604 OID 106070)
+-- TOC entry 5472 (class 2604 OID 106070)
 -- Name: srtm rid; Type: DEFAULT; Schema: public; Owner: geokrety
 --
 
@@ -108,7 +142,7 @@ ALTER TABLE ONLY public.srtm ALTER COLUMN rid SET DEFAULT nextval('public.srtm_r
 
 
 --
--- TOC entry 5451 (class 2606 OID 505903)
+-- TOC entry 5479 (class 2606 OID 505903)
 -- Name: countries countries_pk; Type: CONSTRAINT; Schema: public; Owner: geokrety
 --
 
@@ -117,7 +151,7 @@ ALTER TABLE ONLY public.countries
 
 
 --
--- TOC entry 5447 (class 2606 OID 106072)
+-- TOC entry 5475 (class 2606 OID 106072)
 -- Name: srtm srtm_pkey; Type: CONSTRAINT; Schema: public; Owner: geokrety
 --
 
@@ -126,7 +160,7 @@ ALTER TABLE ONLY public.srtm
 
 
 --
--- TOC entry 5452 (class 1259 OID 505901)
+-- TOC entry 5480 (class 1259 OID 505901)
 -- Name: geog; Type: INDEX; Schema: public; Owner: geokrety
 --
 
@@ -134,7 +168,7 @@ CREATE INDEX geog ON public.countries USING gist (geog);
 
 
 --
--- TOC entry 5448 (class 1259 OID 106074)
+-- TOC entry 5476 (class 1259 OID 106074)
 -- Name: srtm_st_convexhull_idx; Type: INDEX; Schema: public; Owner: geokrety
 --
 
@@ -142,14 +176,14 @@ CREATE INDEX srtm_st_convexhull_idx ON public.srtm USING gist (public.st_convexh
 
 
 --
--- TOC entry 5449 (class 1259 OID 106075)
+-- TOC entry 5477 (class 1259 OID 106075)
 -- Name: srtm_st_convexhull_idx1; Type: INDEX; Schema: public; Owner: geokrety
 --
 
 CREATE INDEX srtm_st_convexhull_idx1 ON public.srtm USING gist (public.st_convexhull(rast));
 
 
--- Completed on 2020-04-15 19:00:57 CEST
+-- Completed on 2020-04-17 17:51:41 CEST
 
 --
 -- PostgreSQL database dump complete
