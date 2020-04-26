652f6518c831be40873a1a52359f673e32ff3c04
Mathieu <github@kumy.net>
2020-04-27 00:12:54 +0200

    FIXUPfilter

--- website/app/GeoKrety/Controller/Cli/PicturesImporter.php
+++ website/app/GeoKrety/Controller/Cli/PicturesImporter.php
@@ -31,7 +31,7 @@ class PicturesImporter extends BaseCleaner {
     }
 
     protected function filterHook() {
-        return ['bucket = ? AND key = ?', NULL, NULL];
+        return ['bucket = ? AND key = ?', null, null];
     }
 
     protected function orderHook() {
