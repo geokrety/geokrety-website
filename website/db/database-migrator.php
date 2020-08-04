dc6d6b1a18354edc691a477df23a14eab901824c
Mathieu <github@kumy.net>
2020-08-04 17:00:20 +0200

    Stop db-migrator clearing db migrations status

--- website/db/database-migrator.php
+++ website/db/database-migrator.php
@@ -45,7 +45,7 @@ $pgsql->query('CREATE TABLE gk_pictures2 AS SELECT filename, bucket, key FROM gk
 $pgsql->query('CREATE INDEX tmp_idx_pictures_filename ON geokrety.gk_pictures2 USING btree (filename);');
 
 $pgsql->query('SET session_replication_role = replica;');
-$sql = 'TRUNCATE "gk_waypoints_gc", "gk_statistics_counters", "gk_statistics_daily_counters", "gk_account_activation", "gk_badges", "gk_email_activation", "gk_geokrety", "gk_geokrety_rating", "gk_mails", "gk_moves_comments", "gk_moves", "gk_news", "gk_news_comments", "gk_news_comments_access", "gk_owner_codes", "gk_password_tokens", "gk_pictures", "gk_races", "gk_races_participants", "gk_users", "gk_watched", "gk_waypoints_oc", "gk_waypoints_country", "gk_waypoints_sync", "gk_waypoints_types", "phinxlog", "scripts", "sessions" RESTART IDENTITY CASCADE';
+$sql = 'TRUNCATE "gk_waypoints_gc", "gk_statistics_counters", "gk_statistics_daily_counters", "gk_account_activation", "gk_badges", "gk_email_activation", "gk_geokrety", "gk_geokrety_rating", "gk_mails", "gk_moves_comments", "gk_moves", "gk_news", "gk_news_comments", "gk_news_comments_access", "gk_owner_codes", "gk_password_tokens", "gk_pictures", "gk_races", "gk_races_participants", "gk_users", "gk_watched", "gk_waypoints_oc", "gk_waypoints_country", "gk_waypoints_sync", "gk_waypoints_types", "scripts", "sessions" RESTART IDENTITY CASCADE';
 $pgsql->query($sql);
 
 // ---------------------------------------------------------------------------------------------------------------------
