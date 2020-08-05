e35a8ae7477f10d15088a995019be2fab63994f2
Mathieu <github@kumy.net>
2020-08-05 13:34:21 +0200

    Fix unit testing pgtap

---
+++ website/db/dumps/phinxlogs.data.sql
@@ -0,0 +1,8 @@
+-- Adminer 4.7.7 PostgreSQL dump
+
+TRUNCATE "phinxlog";
+INSERT INTO "phinxlog" ("version", "migration_name", "start_time", "end_time", "breakpoint") VALUES
+(20200801131052,	'LabelsLists',	'2020-08-04 16:12:22+00',	'2020-08-04 16:12:22+00',	'0'),
+(20200802195759,	'SessionPersist',	'2020-08-04 16:12:31+00',	'2020-08-04 16:12:31+00',	'0');
+
+-- 2020-08-05 11:30:13.420979+00
