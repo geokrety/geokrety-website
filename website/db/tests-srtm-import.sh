93c04c9bfb7b6f41ebc9fac725428e5b62d1ac9f
Mathieu <github@kumy.net>
2020-08-09 17:00:09 +0200

    First docker in travis test

--- website/db/tests-srtm-import.sh
+++ website/db/tests-srtm-import.sh
@@ -4,6 +4,6 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 tmp_dir=$(mktemp -d -p $DIR -t srtm-XXXXXXXXXX)
 
 wget -4 -i $DIR/tests-srtm-import.txt -P $tmp_dir
-raster2pgsql $tmp_dir/*.zip -F -I -e public.srtm | psql -U geokrety
+raster2pgsql $tmp_dir/*.zip -F -I -e public.srtm | psql -U ${PGUSER:-geokrety}
 
 rm -rf $tmp_dir
