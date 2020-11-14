4fb0c7de34eaace94f64a61641f832d4e08d6964
Mathieu <github@kumy.net>
2020-11-14 14:19:43 +0100

    Add link to GK in near home minimap

--- website/app-templates/smarty/js/users/geokrety_near_home_map.tpl.js
+++ website/app-templates/smarty/js/users/geokrety_near_home_map.tpl.js
@@ -4,11 +4,13 @@
 map = initializeMap();
 
 function onEachFeature(feature, layer) {
+    let geokret_link = "{'geokret_details'|alias:'gkid=%GKID%'}".replace('%GKID%', feature.properties.gkid);
     layer.bindPopup(`
                 <div class="scaled">
-                    <h6>${ feature.properties.name }</h6>
+                    <h4><a href="${ geokret_link }">${ feature.properties.name }</a></h4>
                     <dl class="dl-horizontal">
-                        <dt>{t}Waypoint:{/t}</dt><dd>${ feature.properties.waypoint === null ? feature.properties.lat + '/' + feature.properties.lon : feature.properties.waypoint }</dd>
+                        <dt title="{t}Waypoint:{/t}">{t}Waypoint:{/t}</dt><dd>${ feature.properties.waypoint === null ? feature.properties.lat + '/' + feature.properties.lon : feature.properties.waypoint }</dd>
+                        <dt>{t}Date:{/t}</dt><dd title="${ moment.utc(feature.properties.moved_on_datetime).local() }">${ moment.utc(feature.properties.moved_on_datetime).local().fromNow() }</dd>
                     </dl>
                 </div>
             `, {
