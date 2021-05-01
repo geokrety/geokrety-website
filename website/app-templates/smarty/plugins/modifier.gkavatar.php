726a0e31812f3e07617493d7d98c8891868707f4
Mathieu <github@kumy.net>
2021-05-01 12:19:34 +0000

    Fix #629: GK has-avatar popup
    [skip ci]

--- website/app-templates/smarty/plugins/modifier.gkavatar.php
+++ website/app-templates/smarty/plugins/modifier.gkavatar.php
@@ -1,5 +1,7 @@
 <?php
 
+require_once SMARTY_PLUGINS_DIR.'modifier.escape.php';
+
 /*
  * Smarty plugin
  * -------------------------------------------------------------
@@ -9,16 +11,22 @@
  * Purpose:  outputs a geokrety icon based if the GK has an avatar
  * -------------------------------------------------------------
  */
-function smarty_modifier_gkavatar(GeoKrety\Model\Geokret $geokret) {
+function smarty_modifier_gkavatar(GeoKrety\Model\Geokret $geokret): string {
     if (!$geokret->avatar) {
-        return;
+        return '';
     }
 
     $iconUrl = GK_CDN_ICONS_URL.'/idcard.png';
     $alt = _('has avatar icon');
     $title = _('GeoKret has an avatar');
 
-    return <<< EOT
-<img src="$iconUrl" class="img-fluid w-3" width="14" height="10" alt="$alt" title="$title" />
+    $html = <<< EOT
+<a class="has-gk-avatar" href="{$geokret->avatar->url}" title="%s">
+    <img src="$iconUrl" class="img-fluid w-3" width="14" height="10" alt="$alt" title="$title" />
+</a>
 EOT;
+    return sprintf(
+        $html,
+        smarty_modifier_escape(sprintf(_('GeoKret "%s" by %s'), $geokret->name, $geokret->avatar->author->username)),
+    );
 }
