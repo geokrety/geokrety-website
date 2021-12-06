088ce92effe68e13a644637a5560065aaf062de9
Mathieu <github@kumy.net>
2021-12-06 22:42:23 +0100

    Fix db unit tests

--- website/db/tests/test-50-geokret-tracking-code.sql
+++ website/db/tests/test-50-geokret-tracking-code.sql
@@ -2,7 +2,7 @@
 
 BEGIN;
 
-SELECT * FROM no_plan();
+SELECT plan(33);
 
 -- Automatic
 INSERT INTO "gk_geokrety" ("id", "gkid", "name", "type") VALUES (1, 1, 'test 1', 0);
@@ -37,29 +37,29 @@ SELECT is(tracking_code, 'JIOKLJ', 'Tracking code will be saved uppercase') from
 -- cannot start by GK or other networks prefixes
 -- GK, GC, OP, OK, GE, OZ, OU, ON, OL, OJ, OS, GD, GA, VI, MS, TR, EX, GR, RH, OX, OB, OR, LT, LV
 SELECT throws_ok($$INSERT INTO "gk_geokrety" ("id", "gkid", "tracking_code", "name", "type") VALUES (8, 8, 'GK1234', 'test GK', 0)$$);
-SELECT throws_ok($$INSERT INTO "gk_geokrety" ("id", "gkid", "tracking_code", "name", "type") VALUES (9, 9, 'GC', 'test GC', 0)$$);
-SELECT throws_ok($$INSERT INTO "gk_geokrety" ("id", "gkid", "tracking_code", "name", "type") VALUES (10, 10, 'OP', 'test OP', 0)$$);
-SELECT throws_ok($$INSERT INTO "gk_geokrety" ("id", "gkid", "tracking_code", "name", "type") VALUES (11, 11, 'OK', 'test OK', 0)$$);
-SELECT throws_ok($$INSERT INTO "gk_geokrety" ("id", "gkid", "tracking_code", "name", "type") VALUES (12, 12, 'GE', 'test GE', 0)$$);
-SELECT throws_ok($$INSERT INTO "gk_geokrety" ("id", "gkid", "tracking_code", "name", "type") VALUES (13, 13, 'OZ', 'test OZ', 0)$$);
-SELECT throws_ok($$INSERT INTO "gk_geokrety" ("id", "gkid", "tracking_code", "name", "type") VALUES (14, 14, 'OU', 'test OU', 0)$$);
-SELECT throws_ok($$INSERT INTO "gk_geokrety" ("id", "gkid", "tracking_code", "name", "type") VALUES (15, 15, 'ON', 'test ON', 0)$$);
-SELECT throws_ok($$INSERT INTO "gk_geokrety" ("id", "gkid", "tracking_code", "name", "type") VALUES (16, 16, 'OL', 'test OL', 0)$$);
-SELECT throws_ok($$INSERT INTO "gk_geokrety" ("id", "gkid", "tracking_code", "name", "type") VALUES (17, 17, 'OJ', 'test OJ', 0)$$);
-SELECT throws_ok($$INSERT INTO "gk_geokrety" ("id", "gkid", "tracking_code", "name", "type") VALUES (18, 18, 'OS', 'test OS', 0)$$);
-SELECT throws_ok($$INSERT INTO "gk_geokrety" ("id", "gkid", "tracking_code", "name", "type") VALUES (19, 19, 'GD', 'test GD', 0)$$);
-SELECT throws_ok($$INSERT INTO "gk_geokrety" ("id", "gkid", "tracking_code", "name", "type") VALUES (20, 20, 'GA', 'test GA', 0)$$);
-SELECT throws_ok($$INSERT INTO "gk_geokrety" ("id", "gkid", "tracking_code", "name", "type") VALUES (21, 21, 'VI', 'test VI', 0)$$);
-SELECT throws_ok($$INSERT INTO "gk_geokrety" ("id", "gkid", "tracking_code", "name", "type") VALUES (22, 22, 'MS', 'test MS', 0)$$);
-SELECT throws_ok($$INSERT INTO "gk_geokrety" ("id", "gkid", "tracking_code", "name", "type") VALUES (23, 23, 'TR', 'test TR', 0)$$);
-SELECT throws_ok($$INSERT INTO "gk_geokrety" ("id", "gkid", "tracking_code", "name", "type") VALUES (24, 24, 'EX', 'test EX', 0)$$);
-SELECT throws_ok($$INSERT INTO "gk_geokrety" ("id", "gkid", "tracking_code", "name", "type") VALUES (25, 25, 'GR', 'test GR', 0)$$);
-SELECT throws_ok($$INSERT INTO "gk_geokrety" ("id", "gkid", "tracking_code", "name", "type") VALUES (26, 26, 'RH', 'test RH', 0)$$);
-SELECT throws_ok($$INSERT INTO "gk_geokrety" ("id", "gkid", "tracking_code", "name", "type") VALUES (27, 27, 'OX', 'test OX', 0)$$);
-SELECT throws_ok($$INSERT INTO "gk_geokrety" ("id", "gkid", "tracking_code", "name", "type") VALUES (28, 28, 'OB', 'test OB', 0)$$);
-SELECT throws_ok($$INSERT INTO "gk_geokrety" ("id", "gkid", "tracking_code", "name", "type") VALUES (29, 29, 'OR', 'test OR', 0)$$);
-SELECT throws_ok($$INSERT INTO "gk_geokrety" ("id", "gkid", "tracking_code", "name", "type") VALUES (30, 30, 'LT', 'test LT', 0)$$);
-SELECT throws_ok($$INSERT INTO "gk_geokrety" ("id", "gkid", "tracking_code", "name", "type") VALUES (31, 31, 'LV', 'test LV', 0)$$);
+SELECT throws_ok($$INSERT INTO "gk_geokrety" ("id", "gkid", "tracking_code", "name", "type") VALUES (9, 9, 'GC1234', 'test GC', 0)$$);
+SELECT throws_ok($$INSERT INTO "gk_geokrety" ("id", "gkid", "tracking_code", "name", "type") VALUES (10, 10, 'OP1234', 'test OP', 0)$$);
+SELECT throws_ok($$INSERT INTO "gk_geokrety" ("id", "gkid", "tracking_code", "name", "type") VALUES (11, 11, 'OK1234', 'test OK', 0)$$);
+SELECT throws_ok($$INSERT INTO "gk_geokrety" ("id", "gkid", "tracking_code", "name", "type") VALUES (12, 12, 'GE1234', 'test GE', 0)$$);
+SELECT throws_ok($$INSERT INTO "gk_geokrety" ("id", "gkid", "tracking_code", "name", "type") VALUES (13, 13, 'OZ1234', 'test OZ', 0)$$);
+SELECT throws_ok($$INSERT INTO "gk_geokrety" ("id", "gkid", "tracking_code", "name", "type") VALUES (14, 14, 'OU1234', 'test OU', 0)$$);
+SELECT throws_ok($$INSERT INTO "gk_geokrety" ("id", "gkid", "tracking_code", "name", "type") VALUES (15, 15, 'ON1234', 'test ON', 0)$$);
+SELECT throws_ok($$INSERT INTO "gk_geokrety" ("id", "gkid", "tracking_code", "name", "type") VALUES (16, 16, 'OL1234', 'test OL', 0)$$);
+SELECT throws_ok($$INSERT INTO "gk_geokrety" ("id", "gkid", "tracking_code", "name", "type") VALUES (17, 17, 'OJ1234', 'test OJ', 0)$$);
+SELECT throws_ok($$INSERT INTO "gk_geokrety" ("id", "gkid", "tracking_code", "name", "type") VALUES (18, 18, 'OS1234', 'test OS', 0)$$);
+SELECT throws_ok($$INSERT INTO "gk_geokrety" ("id", "gkid", "tracking_code", "name", "type") VALUES (19, 19, 'GD1234', 'test GD', 0)$$);
+SELECT throws_ok($$INSERT INTO "gk_geokrety" ("id", "gkid", "tracking_code", "name", "type") VALUES (20, 20, 'GA1234', 'test GA', 0)$$);
+SELECT throws_ok($$INSERT INTO "gk_geokrety" ("id", "gkid", "tracking_code", "name", "type") VALUES (21, 21, 'VI1234', 'test VI', 0)$$);
+SELECT throws_ok($$INSERT INTO "gk_geokrety" ("id", "gkid", "tracking_code", "name", "type") VALUES (22, 22, 'MS1234', 'test MS', 0)$$);
+SELECT throws_ok($$INSERT INTO "gk_geokrety" ("id", "gkid", "tracking_code", "name", "type") VALUES (23, 23, 'TR1234', 'test TR', 0)$$);
+SELECT throws_ok($$INSERT INTO "gk_geokrety" ("id", "gkid", "tracking_code", "name", "type") VALUES (24, 24, 'EX1234', 'test EX', 0)$$);
+SELECT throws_ok($$INSERT INTO "gk_geokrety" ("id", "gkid", "tracking_code", "name", "type") VALUES (25, 25, 'GR1234', 'test GR', 0)$$);
+SELECT throws_ok($$INSERT INTO "gk_geokrety" ("id", "gkid", "tracking_code", "name", "type") VALUES (26, 26, 'RH1234', 'test RH', 0)$$);
+SELECT throws_ok($$INSERT INTO "gk_geokrety" ("id", "gkid", "tracking_code", "name", "type") VALUES (27, 27, 'OX1234', 'test OX', 0)$$);
+SELECT throws_ok($$INSERT INTO "gk_geokrety" ("id", "gkid", "tracking_code", "name", "type") VALUES (28, 28, 'OB1234', 'test OB', 0)$$);
+SELECT throws_ok($$INSERT INTO "gk_geokrety" ("id", "gkid", "tracking_code", "name", "type") VALUES (29, 29, 'OR1234', 'test OR', 0)$$);
+SELECT throws_ok($$INSERT INTO "gk_geokrety" ("id", "gkid", "tracking_code", "name", "type") VALUES (30, 30, 'LT1234', 'test LT', 0)$$);
+SELECT throws_ok($$INSERT INTO "gk_geokrety" ("id", "gkid", "tracking_code", "name", "type") VALUES (31, 31, 'LV1234', 'test LV', 0)$$);
 
 -- Finish the tests and clean up.
 SELECT * FROM finish();
