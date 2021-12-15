7ea32096c5772333b196c45ed3569c424c259f0b
Mathieu <github@kumy.net>
2021-12-15 23:46:57 +0100

    feat: Enable csrf for news comment delete

--- website/app/GeoKrety/Controller/Pages/NewsCommentDelete.php
+++ website/app/GeoKrety/Controller/Pages/NewsCommentDelete.php
@@ -9,10 +9,15 @@ class NewsCommentDelete extends Base {
     use NewsCommentLoader;
 
     public function get(\Base $f3) {
-        Smarty::render('dialog/news_comment_delete.tpl');
+        Smarty::render('extends:full_screen_modal.tpl|dialog/news_comment_delete.tpl');
+    }
+
+    public function get_ajax(\Base $f3) {
+        Smarty::render('extends:base_modal.tpl|dialog/news_comment_delete.tpl');
     }
 
     public function post(\Base $f3) {
+        $this->checkCsrf();
         $comment = $this->comment;
         $newsId = $comment->news->id;
         if ($comment->valid()) {
