d3280ddce5d28c5f919a1e6d5a5f998238d7e51a
Mathieu <github@kumy.net>
2021-12-23 21:03:39 +0100

    feat: Enable csrf for email change validate

--- website/app/GeoKrety/Controller/Pages/UserEmailChange.php
+++ website/app/GeoKrety/Controller/Pages/UserEmailChange.php
@@ -9,9 +9,15 @@ use GeoKrety\Service\Smarty;
 class UserEmailChange extends Base {
     public function get(\Base $f3) {
         $token = new EmailActivationToken();
+        Smarty::assign('token', $token);
 
         // Check database for provided token
         if ($f3->exists('POST.token')) {
+            $this->checkCsrf(function ($error) {
+                Flash::instance()->addMessage($error, 'danger');
+                Smarty::render('pages/email_change.tpl');
+                exit();
+            });
             $token->load(['token = ? AND used = ? AND created_on_datetime > NOW() - cast(? as interval)', $f3->get('POST.token'), EmailActivationToken::TOKEN_UNUSED, GK_SITE_EMAIL_ACTIVATION_CODE_DAYS_VALIDITY.' DAY']);
             if ($token->valid()) {
                 $f3->reroute(sprintf('@user_update_email_validate_token(@token=%s)', $f3->get('POST.token')));
@@ -20,7 +26,6 @@ class UserEmailChange extends Base {
             $token->token = $f3->get('POST.token');
         }
 
-        Smarty::assign('token', $token);
         Smarty::render('pages/email_change.tpl');
     }
 }
