efb6e7376bd07ac201d8e6ae78443aa70455fda1
Mathieu <github@kumy.net>
2022-02-25 16:39:58 +0100

    fix: Create missing index on moves table

--- website/db/dumps/README.md
+++ website/db/dumps/README.md
@@ -1,5 +1,5 @@
 
-In order to initialize  database, you may need initial data for `public.countries`
-and `public.timezones`. As this file is really huge, it's is hosted outside of the
+In order to initialize the database, you may need initial data for `public.countries`
+and `public.timezones`. As this file is really huge, it's hosted outside the
 git repo. You may get it using direct link https://srtm.geokrety.org/11_public-data.sql.gz
-or using script `get_huge_files.sh`.
+or using script `get_huge_dump_files.sh`.
---
+++ website/db/migrations/20220204183524_moves_indexes.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+use Phinx\Migration\AbstractMigration;
+
+final class MovesIndexes extends AbstractMigration {
+    public function up(): void {
+        $this->execute('CREATE INDEX "gk_moves_moved_on_datetime" ON "gk_moves" ("moved_on_datetime")');
+        $this->execute('CREATE INDEX "gk_moves_created_on_datetime" ON "gk_moves" ("created_on_datetime")');
+        $this->execute('CREATE INDEX "gk_moves_updated_on_datetime" ON "gk_moves" ("updated_on_datetime")');
+        $this->execute('CREATE INDEX "gk_moves_geokret_id" ON "gk_moves" ("geokret")');
+        $this->execute('CREATE INDEX "gk_moves_country" ON "gk_moves" ("country")');
+        $this->execute('CREATE INDEX "gk_moves_move_type" ON "gk_moves" ("move_type")');
+        $this->execute('CREATE INDEX "gk_moves_move_type_id_position" ON "gk_moves" ("move_type", "id", "position")');
+        //$this->execute('CREATE INDEX "gk_moves_move_type_distance" ON "gk_moves" ("move_type", "distance")');
+        $this->execute('CREATE INDEX "gk_moves_elevation" ON "gk_moves" ("elevation")');
+        $this->execute('CREATE INDEX "gk_moves_lat" ON "gk_moves" ("lat")');
+        $this->execute('CREATE INDEX "gk_moves_lon" ON "gk_moves" ("lon")');
+        $this->execute('CREATE INDEX "gk_moves_author" ON "gk_moves" ("author")');
+        $this->execute('CREATE INDEX "gk_moves_waypoint" ON "gk_moves" ("waypoint")');
+        $this->execute('CREATE INDEX "gk_moves_move_type_id" ON "gk_moves" ("move_type", "id")');
+    }
+
+    public function down(): void {
+        $this->execute('DROP INDEX "gk_moves_moved_on_datetime"');
+        $this->execute('DROP INDEX "gk_moves_created_on_datetime"');
+        $this->execute('DROP INDEX "gk_moves_updated_on_datetime"');
+        $this->execute('DROP INDEX "gk_moves_geokret_id"');
+        $this->execute('DROP INDEX "gk_moves_country"');
+        $this->execute('DROP INDEX "gk_moves_move_type"');
+        $this->execute('DROP INDEX "gk_moves_move_type_id_position"');
+        //$this->execute('DROP INDEX "gk_moves_move_type_distance"');
+        $this->execute('DROP INDEX "gk_moves_elevation"');
+        $this->execute('DROP INDEX "gk_moves_lat"');
+        $this->execute('DROP INDEX "gk_moves_lon"');
+        $this->execute('DROP INDEX "gk_moves_author"');
+        $this->execute('DROP INDEX "gk_moves_waypoint"');
+        $this->execute('DROP INDEX "gk_moves_move_type_id"');
+    }
+}
