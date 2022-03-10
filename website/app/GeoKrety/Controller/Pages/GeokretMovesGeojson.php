6096976bb8fdb8982d70d584222c2b9141235ce6
Mathieu <github@kumy.net>
2022-03-10 10:40:13 +0100

    perf: Enhance a geojson database query

--- website/app/GeoKrety/Controller/Pages/GeokretMovesGeojson.php
+++ website/app/GeoKrety/Controller/Pages/GeokretMovesGeojson.php
@@ -17,46 +17,39 @@ class GeokretMovesGeojson extends Base {
         $per_page = GK_PAGINATION_GEOKRET_MOVES;
 
         $sql = <<<EOT
+WITH waypoints AS (
+    SELECT gk_moves.id AS move_id, gk_moves.*, COALESCE(gk_users.username, gk_moves.username) AS author_username,
+        ROW_NUMBER() OVER (ORDER BY moved_on_datetime ASC) AS step
+    FROM gk_moves
+    LEFT JOIN gk_users ON author = gk_users.id
+    WHERE geokret=?
+    AND position is not NULL
+    ORDER BY moved_on_datetime DESC
+    LIMIT ?
+    OFFSET ?
+),
+t AS (
+    select waypoint, position, elevation, distance, country, step,
+        COALESCE(TRUNC(EXTRACT(EPOCH FROM (moved_on_datetime - lag(moved_on_datetime) over (order by moved_on_datetime DESC)::timestamptz))/60), 0) AS minutes,
+        lat, lon,
+        moved_on_datetime,
+        move_id,
+        move_type,
+        author, author_username,
+        COALESCE (lag(step) over (order by moved_on_datetime DESC), step) AS next_step,
+        COALESCE (lag(step) over (order by moved_on_datetime ASC), step) AS previous_step
+    from waypoints
+)
             select json_build_object(
                 'type', 'FeatureCollection',
                 'features', COALESCE(json_agg(public.ST_AsGeoJSON(t.*)::json), '[]')::jsonb || COALESCE(json_agg(public.ST_AsGeoJSON(f.*)::json), '[]')::jsonb
             ) AS geojson
-            from (select waypoint, position, elevation, distance, country, step,
-                coalesce(TRUNC(EXTRACT(EPOCH FROM (moved_on_datetime - lag(moved_on_datetime) over (order by moved_on_datetime DESC)::timestamptz))/60), 0) AS minutes,
-                lat, lon,
-                moved_on_datetime,
-                move_id,
-                move_type,
-                author, author_username,
-                COALESCE (lag(step) over (order by moved_on_datetime DESC), step) AS next_step,
-                COALESCE (lag(step) over (order by moved_on_datetime ASC), step) AS previous_step
-                from (
-                    SELECT gk_moves.id AS move_id, gk_moves.*, COALESCE(gk_users.username, gk_moves.username) AS author_username,
-                        ROW_NUMBER() OVER (ORDER BY moved_on_datetime ASC) AS step
-                    FROM gk_moves
-                    LEFT JOIN gk_users ON author = gk_users.id
-                    WHERE geokret=?
-                    ORDER BY moved_on_datetime DESC
-                    LIMIT ?
-                    OFFSET ?
-                ) as y
-                WHERE y.position is not NULL
-            ) as t,
-            (SELECT public.ST_MakeLine(position::public.geometry ORDER BY moved_on_datetime DESC) As geom
-                FROM (
-                    SELECT id, geokret, position, moved_on_datetime
-                    FROM gk_moves
-                    where geokret=?
-                    GROUP BY id
-                    ORDER BY moved_on_datetime DESC
-                    LIMIT ?
-                    OFFSET ?
-                ) AS g
-                WHERE g.position is not NULL
-                GROUP BY g.geokret
+            from t,
+            (SELECT public.ST_MakeLine(position::public.geometry) As geom
+                FROM waypoints
             ) as f;
 EOT;
-        $result = $f3->get('DB')->exec($sql, [$this->geokret->id, $per_page, $start, $this->geokret->id, $per_page, $start]);
+        $result = $f3->get('DB')->exec($sql, [$this->geokret->id, 10000, $start]);
         exit($result[0]['geojson']);
     }
 }
