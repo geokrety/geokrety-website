geokrety/geokrety-website

commit ee6bf1b6b9a6abec4e218e9f77840e9e9f91da6c

fix: Fix create account via OAuth2

website/app/GeoKrety/Model/User.php
@@ -5,6 +5,7 @@ namespace GeoKrety\Model;
 use DateTime;
 use DB\CortexCollection;
 use DB\SQL\Schema;
+use GeoKrety\Email\AccountActivation;
 use JsonSerializable;
 
 /**
@@ -307,22 +308,19 @@ class User extends Base implements JsonSerializable {
     }
 
     public function sendAccountActivationEmail(): void {
-        if (is_null($this->email)) {
+        if (is_null($this->email) or $this->isAccountValid()) {
             // skip sending mail
             return;
         }
         $token = new AccountActivationToken();
         $token->user = $this;
         $token->save();
-        $smtp = new \GeoKrety\Email\AccountActivation();
+        $smtp = new AccountActivation();
         $smtp->sendActivation($token);
     }
 
-    /**
-     * @param false $flash_msg_only get only messages, do not send mail
-     */
-    public function resendAccountActivationEmail($flash_msg_only = false): void {
-        if (is_null($this->email)) {
+    public function resendAccountActivationEmail(): void {
+        if (is_null($this->email) or $this->isEmailValid()) {
             // skip sending mail
             return;
         }
@@ -333,12 +331,6 @@ class User extends Base implements JsonSerializable {
             $token->set_email($this->get_email());
             $token->save();
             $smtp = new \GeoKrety\Email\EmailRevalidate();
-            if ($flash_msg_only) {
-                $smtp->prepareMessageSendRevalidation();
-                $smtp->flashMessage($token);
-
-                return;
-            }
             $smtp->sendRevalidation($token);
 
             return;
@@ -350,16 +342,8 @@ class User extends Base implements JsonSerializable {
                 $token->user = $this;
                 $token->save();
             }
-            $smtp = new \GeoKrety\Email\AccountActivation();
-            if ($flash_msg_only) {
-                $smtp->prepareMessageActivationAgainOnLogin();
-                $smtp->flashMessage($token);
-
-                return;
-            }
+            $smtp = new AccountActivation();
             $smtp->sendActivationAgainOnLogin($token);
-
-            return;
         }
     }
 

website/app/GeoKrety/Controller/Pages/Login.php
@@ -88,10 +88,6 @@ class Login extends Base {
             $user->resendAccountActivationEmail();
             exit();
         }
-        $user->resendAccountActivationEmail(true);
-        if (!GK_DEVEL) {
-            $f3->abort();
-        }
         $user->resendAccountActivationEmail();
     }
 

website/app/authorizations.ini
@@ -27,7 +27,6 @@ deny @registration = authenticated,admin,superadmin
 allow @registration_social = *
 deny @registration_social = authenticated,admin,superadmin
 allow @registration_activate = *
-deny @registration_activate = authenticated,admin,superadmin
 
 allow @api_v1_login2secid = *
 

commit 6d1d61c055a9e4c93eceab0716f8ddcd0ba4ade9

fix: Fix small bug introduced by ee6bf1b6

website/app/GeoKrety/Model/User.php
@@ -34,6 +34,11 @@ use JsonSerializable;
  * @property int account_valid
  * @property AccountActivationToken activation
  * @property CortexCollection social_auth
+ * @property CortexCollection moves
+ * @property CortexCollection moves_comments
+ * @property CortexCollection geokrety_owned
+ * @property CortexCollection geokrety_held
+ * @property CortexCollection yearly_ranks
  */
 class User extends Base implements JsonSerializable {
     // Validation occurs in validate() for this class
@@ -320,7 +325,7 @@ class User extends Base implements JsonSerializable {
     }
 
     public function resendAccountActivationEmail(): void {
-        if (is_null($this->email) or $this->isEmailValid()) {
+        if (is_null($this->email)) {
             // skip sending mail
             return;
         }
@@ -335,6 +340,7 @@ class User extends Base implements JsonSerializable {
 
             return;
         }
+
         if ($this->isAccountInvalid()) {
             $token = new AccountActivationToken();
             $token->loadUserActiveToken($this);