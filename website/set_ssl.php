aa4ae3f1a8daa849ef1682172ff041417c6c1c46
Mathieu <github@kumy.net>
2022-06-07 20:12:58 +0200

    Fix redirection loop to ssl

--- website/set_ssl.php
+++ website/set_ssl.php
@@ -1,15 +1,3 @@
 <?php
 
-require_once '__sentry.php';
-
-//==== Turn on HTTPS - Detect if HTTPS, if not on, then turn on HTTPS:
-if ($_SERVER['HTTPS'] != 'on') {
-    header('Location: '.strtr($_SERVER['HTTP_REFERER'], ['http://' => 'https://']));
-    exit;
-} else {
-    header('Location: '.$_SERVER['HTTP_REFERER']);
-} exit;
-
-    //if($_SERVER['HTTPS'] == 'on'){
-    //    header("Location: " . strtr($_SERVER["HTTP_REFERER"], array('https://' => 'http://'))); exit;
-    //}
+header('Location: /');
