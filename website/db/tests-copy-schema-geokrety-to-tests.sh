880c08cdb2d1e83e417656125e0fd36090972e2d
Mathieu <github@kumy.net>
2022-06-18 18:24:18 +0200

    fix: ADd new schema in copy scripts

--- website/db/tests-copy-schema-geokrety-to-tests.sh
+++ website/db/tests-copy-schema-geokrety-to-tests.sh
@@ -5,6 +5,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 tmp_dir=$(mktemp -d -p $DIR -t copy-schema-XXXXXXXXXX)
 
 pg_dump --file $tmp_dir/geokrety-schema.sql --host "localhost" --port "5432" --username "geokrety" --format=p --schema-only --encoding "UTF8" --schema "geokrety" "geokrety"
+pg_dump --file $tmp_dir/audit-schema.sql --host "localhost" --port "5432" --username "geokrety" --format=p --schema-only --encoding "UTF8" --schema "audit" "geokrety"
 
 cat << EOF | psql -U geokrety -h localhost tests
 BEGIN;
@@ -14,6 +15,8 @@ DROP SCHEMA geokrety CASCADE;
 CREATE EXTENSION quantile WITH SCHEMA geokrety;
 REFRESH MATERIALIZED VIEW "geokrety"."gk_geokrety_in_caches";
 
+DROP SCHEMA IF EXISTS audit CASCADE;
+\i $tmp_dir/audit-schema.sql
 END;
 EOF
 
--- website/db/tests-copy-schema-tests-to-geokrety.sh
+++ website/db/tests-copy-schema-tests-to-geokrety.sh
@@ -5,15 +5,19 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 tmp_dir=$(mktemp -d -p $DIR -t copy-schema-XXXXXXXXXX)
 
 pg_dump --file $tmp_dir/geokrety-schema.sql --host "localhost" --port "5432" --username "geokrety" --format=p --schema-only --encoding "UTF8" --schema "geokrety" "tests"
+pg_dump --file $tmp_dir/audit-schema.sql --host "localhost" --port "5432" --username "geokrety" --format=p --schema-only --encoding "UTF8" --schema "audit" "tests"
 pg_dump --file $tmp_dir/geokrety-data.tar --host "localhost" --port "5432" --username "geokrety" --format=t --blobs --data-only --encoding "UTF8" --schema "geokrety" "geokrety"
 
 cat << EOF | psql -U geokrety -h localhost geokrety
 BEGIN;
 
-DROP SCHEMA geokrety CASCADE;
+DROP SCHEMA IF EXISTS geokrety CASCADE;
 \i $tmp_dir/geokrety-schema.sql
 CREATE EXTENSION quantile WITH SCHEMA geokrety;
 
+
+DROP SCHEMA IF EXISTS audit CASCADE;
+\i $tmp_dir/audit-schema.sql
 END;
 EOF
 pg_restore -U geokrety --host "localhost" --dbname "geokrety" --data-only --disable-triggers --schema "geokrety" $tmp_dir/geokrety-data.tar
