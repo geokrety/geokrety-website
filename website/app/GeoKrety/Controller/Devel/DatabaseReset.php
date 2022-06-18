2f1a94bc058d0eb6685cb2632ed113c6f19fe56d
Mathieu <github@kumy.net>
2022-06-18 20:40:02 +0200

    Fix: Fix move audit sequence to audit schema

--- website/app/GeoKrety/Controller/Devel/DatabaseReset.php
+++ website/app/GeoKrety/Controller/Devel/DatabaseReset.php
@@ -9,7 +9,7 @@ use Exception;
  */
 class DatabaseReset extends Base {
     public const TABLES = [
-        'gk_account_activation', 'audits.actions_logs', 'audits.posts', 'gk_awards_won', 'gk_email_activation',
+        'gk_account_activation', 'audit.actions_logs', 'audit.posts', 'gk_awards_won', 'gk_email_activation',
         'gk_geokrety', 'gk_geokrety_rating', 'gk_mails',
         'gk_moves', 'gk_moves_comments', 'gk_news', 'gk_news_comments',
         'gk_news_comments_access', 'gk_owner_codes', 'gk_password_tokens', 'gk_pictures',
@@ -26,8 +26,8 @@ class DatabaseReset extends Base {
         'gk_statistics_daily_counters_id_seq', 'gk_users_social_auth_id_seq',
         'mails_id_seq', 'move_comments_id_seq', 'moves_id_seq',
         'news_comments_access_id_seq', 'news_comments_id_seq', 'news_id_seq',
-        'owner_codes_id_seq', 'password_tokens_id_seq', 'audits.posts_id_seq', 'pictures_id_seq',
-        'races_id_seq', 'races_participants_id_seq', 'audits.audit_logs_id_seq',
+        'owner_codes_id_seq', 'password_tokens_id_seq', 'audit.posts_id_seq', 'pictures_id_seq',
+        'races_id_seq', 'races_participants_id_seq', 'audit.audit_logs_id_seq',
         'users_id_seq', 'watched_id_seq', 'waypoints_oc_id_seq', 'waypoints_gc_id_seq',
     ];
 
