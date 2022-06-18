4981c08d5a29d6f20fd697f4d683027736ea72ff
Mathieu <github@kumy.net>
2022-06-18 17:53:56 +0200

    fix: Fix refresh secid

---
+++ website/db/migrations/20220618124635_secid_length.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+use Phinx\Migration\AbstractMigration;
+
+final class SecidLenght extends AbstractMigration {
+    public function up(): void {
+        $sql = <<<'EOL'
+CREATE OR REPLACE FUNCTION geokrety.generate_secid(IN size integer DEFAULT 128)
+    RETURNS character varying
+    LANGUAGE 'sql'
+    VOLATILE
+    PARALLEL UNSAFE
+    COST 100
+
+AS $BODY$SELECT array_to_string(array(select substr('ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyz',((random()*(62-1)+1)::integer),1) from generate_series(1,size)),'');$BODY$;
+EOL;
+        $this->execute($sql);
+    }
+
+    public function down(): void {
+        $sql = <<<'EOL'
+CREATE OR REPLACE FUNCTION geokrety.generate_secid(IN size integer DEFAULT 42)
+    RETURNS character varying
+    LANGUAGE 'sql'
+    VOLATILE
+    PARALLEL UNSAFE
+    COST 100
+
+AS $BODY$SELECT array_to_string(array(select substr('ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyz',((random()*(62-1)+1)::integer),1) from generate_series(1,size)),'');$BODY$;
+EOL;
+        $this->execute($sql);
+    }
+}
