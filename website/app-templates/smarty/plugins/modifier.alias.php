28a955fcb2641312776edcba7ca4c61456b89ae5
Mathieu <github@kumy.net>
2023-08-12 09:11:15 +0200

    fix: Remove workaround as fatfree-core is updated

--- website/app-templates/smarty/plugins/modifier.alias.php
+++ website/app-templates/smarty/plugins/modifier.alias.php
@@ -15,16 +15,6 @@ function smarty_modifier_alias($string, $params = null, $query = null, $fragment
     }
 
     $f3 = \Base::instance();
-    // Workaround for https://github.com/bcosca/fatfree-core/issues/345
-    if (!$f3->exists('__WORKAROUND_1__')) {
-        foreach ($f3->get('PARAMS') as $k => $v) {
-            if ($k === 0) {
-                continue;
-            }
-            $f3->set("PARAMS.$k", urlencode($v));
-        }
-        $f3->set('__WORKAROUND_1__', true);
-    }
 
     return GK_SITE_BASE_SERVER_URL.$f3->alias($string, $params ?? [], $query).$fragment;
 }
