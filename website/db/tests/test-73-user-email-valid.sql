3dc255f898055360f0a718e48fbf3088aac53c41
Mathieu <github@kumy.net>
2024-05-05 11:35:16 +0200

    tests: Fix db test for new email status

--- website/db/tests/test-73-user-email-valid.sql
+++ website/db/tests/test-73-user-email-valid.sql
@@ -3,7 +3,7 @@
 BEGIN;
 
 -- SELECT * FROM no_plan();
-SELECT plan(6);
+SELECT plan(7);
 
 -- const USER_EMAIL_NO_ERROR = 0;
 SELECT lives_ok($$INSERT INTO "gk_users" ("id", "username", "registration_ip", "email_invalid") VALUES (1, 'username1', '127.0.0.1', 0::smallint)$$);
@@ -13,9 +13,11 @@ SELECT lives_ok($$INSERT INTO "gk_users" ("id", "username", "registration_ip", "
 SELECT lives_ok($$INSERT INTO "gk_users" ("id", "username", "registration_ip", "email_invalid") VALUES (3, 'username3', '127.0.0.1', 2::smallint)$$);
 -- const USER_EMAIL_MISSING = 3;
 SELECT lives_ok($$INSERT INTO "gk_users" ("id", "username", "registration_ip", "email_invalid") VALUES (4, 'username4', '127.0.0.1', 3::smallint)$$);
+-- const USER_EMAIL_MAILBOX_FULL = 4;
+SELECT lives_ok($$INSERT INTO "gk_users" ("id", "username", "registration_ip", "email_invalid") VALUES (5, 'username5', '127.0.0.1', 4::smallint)$$);
 
-SELECT throws_ok($$INSERT INTO "gk_users" ("id", "username", "registration_ip", "email_invalid") VALUES (5, 'username5', '127.0.0.1', 4::smallint)$$);
 SELECT throws_ok($$INSERT INTO "gk_users" ("id", "username", "registration_ip", "email_invalid") VALUES (6, 'username6', '127.0.0.1', 5::smallint)$$);
+SELECT throws_ok($$INSERT INTO "gk_users" ("id", "username", "registration_ip", "email_invalid") VALUES (7, 'username7', '127.0.0.1', 6::smallint)$$);
 
 -- Finish the tests and clean up.
 SELECT * FROM finish();
