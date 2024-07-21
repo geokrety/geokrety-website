accfd81d374757d5f3737cede5032bf5570cbd93
Mathieu <github@kumy.net>
2024-07-21 13:30:35 +0200

    fix: Use svg and resize all hall_of_fame icons

--- website/app/GeoKrety/Controller/Pages/HallOfFame.php
+++ website/app/GeoKrety/Controller/Pages/HallOfFame.php
@@ -28,7 +28,8 @@ class HallOfFame extends Base {
         // ];
         $credits[] = [
             'name' => 'php',
-            'icon' => GK_CDN_LOGOS_URL.'/php-power-white.gif',
+            'icon' => GK_CDN_LOGOS_URL.'/php.svg',
+            'icon_width' => '70px',
             'link' => 'https://secure.php.net/',
             'desc' => 'server-side scripting language',
         ];
@@ -37,66 +38,65 @@ class HallOfFame extends Base {
             'desc' => '<a href="https://getcomposer.org/">composer</a>, <a href="https://www.smarty.net/docsv2/fr/">smarty</a>, <a href="https://phpunit.de/">phpunit</a>, <a href="https://github.com/FriendsOfPHP/PHP-CS-Fixer">FriendsOfPHP/PHP-CS-Fixer</a>, <a href="https://github.com/Torann/json-ld">Torann/json-ld</a>',
         ];
         $credits[] = [
-            'name' => 'apache',
-            'icon' => GK_CDN_LOGOS_URL.'/Apache_Server_2.jpg',
+            'name' => 'nginx',
+            'icon' => GK_CDN_LOGOS_URL.'/nginx.svg',
+            'icon_width' => '70px',
             'link' => 'https://httpd.apache.org/',
             'desc' => 'web server application (and extensions)',
         ];
         $credits[] = [
-            'name' => 'mariadb',
-            'icon' => GK_CDN_LOGOS_URL.'/MariaDB-Foundation-vertical-small.png',
-            'link' => 'https://mariadb.org',
-            'desc' => 'relational database management system',
+            'name' => 'Postgresql',
+            'icon' => GK_CDN_LOGOS_URL.'/postgresql.svg',
+            'icon_width' => '70px',
+            'link' => 'https://www.postgresql.org/',
+            'desc' => 'The World\'s Most Advanced Open Source Relational Database',
         ];
         $credits[] = [
             'name' => 'docker',
-            'icon' => GK_CDN_LOGOS_URL.'/vertical.png',
+            'icon' => GK_CDN_LOGOS_URL.'/docker.svg',
+            'icon_width' => '70px',
             'link' => 'https://www.docker.com',
             'desc' => 'operating-system-level virtualization',
         ];
         $credits[] = [
             'name' => 'git',
-            'icon' => GK_CDN_LOGOS_URL.'/2color-lightbg%402x.png',
+            'icon' => GK_CDN_LOGOS_URL.'/git.svg',
+            'icon_width' => '70px',
             'link' => 'https://git-scm.com',
             'desc' => 'source code management',
         ];
         $credits[] = [
             'name' => 'github',
-            'icon' => GK_CDN_LOGOS_URL.'/Octocat.png',
+            'icon' => GK_CDN_LOGOS_URL.'/octocat.svg',
+            'icon_width' => '70px',
             'link' => 'https://github.com',
             'desc' => 'project management, hosting',
         ];
         $credits[] = [
-            'name' => 'travis',
-            'icon' => GK_CDN_LOGOS_URL.'/TravisCI-Mascot-pride.png',
-            'icon_width' => '70px',
-            'link' => 'https://docs.travis-ci.com/',
-            'desc' => 'continuous integration',
-        ];
-        $credits[] = [
             'name' => 'codacy',
-            'icon' => GK_CDN_LOGOS_URL.'/logo-codacy.png',
-            'icon_style' => 'background: rgba(34, 47, 69, 1);',
-            'icon_width' => '130px',
+            'icon' => GK_CDN_LOGOS_URL.'/codacy.svg',
+            'icon_width' => '70px',
             'link' => 'https://opensource.codacy.com',
             'desc' => 'manage code quality',
         ];
         $credits[] = [
             'name' => 'crowdin',
-            'icon' => GK_CDN_LOGOS_URL.'/crowdin-TranslationManagementService-logo.png',
-            'icon_width' => '130px',
+            'icon' => GK_CDN_LOGOS_URL.'/crowdin.svg',
+            'icon_width' => '70px',
             'link' => 'https://crowdin.com',
             'desc' => 'manage translations',
         ];
         $credits[] = [
             'name' => 'adnanh/webhook',
-            'icon' => GK_CDN_LOGOS_URL.'/logo-128x128.png',
+            'icon' => GK_CDN_LOGOS_URL.'/webhook.svg',
+            'icon_width' => '70px',
             'link' => 'https://github.com/adnanh/webhook',
             'desc' => 'configurable incoming webhook server',
         ];
         $credits[] = [
             'name' => 'Sentry',
-            'icon' => GK_CDN_LOGOS_URL.'/sentry-glyph-black.svg',
+            'icon' => GK_CDN_LOGOS_URL.'/sentry.svg',
+            'icon_width' => '70px',
             'link' => 'https://sentry.io/welcome/',
             'desc' => 'Open-source error tracking',
         ];
@@ -107,41 +107,42 @@ class HallOfFame extends Base {
         ];
         $credits[] = [
             'name' => 'Bootstrap',
-            'icon' => GK_CDN_LOGOS_URL.'/bootstrap-solid.svg',
-            'icon_width' => '50px',
+            'icon' => GK_CDN_LOGOS_URL.'/bootstrap.svg',
+            'icon_width' => '70px',
             'link' => 'https://getbootstrap.com/',
             'desc' => 'HTML, CSS, and JS framework',
         ];
         $credits[] = [
             'name' => 'flag-icon-css',
-            'icon' => GK_CDN_LOGOS_URL.'/flag-icon-css.jpg',
-            'link' => 'http://flag-icon-css.lip.is/',
-            'desc' => 'A collection of all country flags in SVG — plus the CSS',
+            'icon' => GK_CDN_LOGOS_URL.'/flag-icon.svg',
+            'icon_width' => '100px',
+            'link' => 'https://flagicons.lipis.dev/',
+            'desc' => 'A curated collection of all country flags in SVG — plus the CSS for easier integration',
         ];
         $credits[] = [
-            'name' => 'openstreetmap',
-            'icon' => GK_CDN_LOGOS_URL.'/Public-images-osm_logo.svg',
+            'name' => 'OpenStreetMap',
+            'icon' => GK_CDN_LOGOS_URL.'/osm.svg',
             'icon_width' => '70px',
             'link' => 'https://openstreetmap.org',
             'desc' => 'Map',
         ];
         $credits[] = [
-            'name' => 'robot framework',
-            'icon' => GK_CDN_LOGOS_URL.'/robot-framework.png',
+            'name' => 'Robot Framework',
+            'icon' => GK_CDN_LOGOS_URL.'/robot-framework.svg',
             'icon_width' => '70px',
             'link' => 'http://robotframework.org/',
             'desc' => 'Automated tests (used by <a href="https://github.com/geokrety/geokrety-website-qa">geokrety-website-qa</a>)',
         ];
         $credits[] = [
             'name' => 'Libravatar',
-            'icon' => 'https://seccdn.libravatar.org/nobody/65.png',
-            'icon_width' => '65px',
+            'icon' => GK_CDN_LOGOS_URL.'/libravatar.svg',
+            'icon_width' => '70px',
             'link' => 'https://www.libravatar.org/',
             'desc' => 'Libravatar is a service which delivers your avatar (profile picture) to other websites.',
         ];
         $credits[] = [
             'name' => 'reCAPTCHA',
-            'icon' => GK_CDN_LOGOS_URL.'/RecaptchaLogo.svg',
+            'icon' => GK_CDN_LOGOS_URL.'/recaptcha.svg',
             'icon_width' => '70px',
             'link' => 'https://www.google.com/recaptcha/',
             'desc' => 'Secure captcha (stop bots)',
@@ -154,14 +155,30 @@ class HallOfFame extends Base {
         $credits[] = [
             'name' => 'Fat-Free Framework',
             'icon' => GK_CDN_LOGOS_URL.'/f3.svg',
-            'icon_width' => '130px',
+            'icon_width' => '70px',
             'link' => 'https://fatfreeframework.com',
             'desc' => 'PHP micro-framework',
         ];
         $credits[] = [
             'name' => 'Google Noto Color Emoji',
+            'icon' => GK_CDN_LOGOS_URL.'/noto-emoji.svg',
+            'icon_width' => '50px',
             'link' => 'https://github.com/googlefonts/noto-emoji',
             'desc' => 'Noto Emoji fonts',
+        ];
+        $credits[] = [
+            'name' => 'SVG Repo',
+            'icon' => GK_CDN_LOGOS_URL.'/svg-repo.svg',
+            'icon_width' => '100px',
+            'link' => 'https://www.svgrepo.com/',
+            'desc' => '500.000+ Open-licensed SVG Vector and Icons',
+        ];
+        $credits[] = [
+            'name' => 'JetBrains',
+            'icon' => GK_CDN_LOGOS_URL.'/jetbrains.svg',
+            'icon_width' => '70px',
+            'link' => 'https://www.jetbrains.com/',
+            'desc' => 'A rich suite of tools that provide an exceptional developer experience',
         ];
         Smarty::assign('app_credits', $credits);
 
