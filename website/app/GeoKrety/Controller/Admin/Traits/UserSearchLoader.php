862f8e4a21140dfaf122d130156c8ab9b8fea068
Mathieu A <github@kumy.net>
2025-04-30 09:37:49 +0200

    fix: order users by id desc in admin

--- website/app/GeoKrety/Controller/Admin/Traits/UserSearchLoader.php
+++ website/app/GeoKrety/Controller/Admin/Traits/UserSearchLoader.php
@@ -14,28 +14,29 @@ trait UserSearchLoader {
 
     public function searchUsers(\Base $f3) {
         $search = $f3->get('GET.search');
+        if (empty($search)) {
+            $search = '%%';
+        }
         Smarty::assign('search', $search);
 
-        if (!empty($search)) {
-            $user = new User();
-            $filter = [
-                'lower(username) like lower(?) OR _email_hash = public.digest(lower(?), \'sha256\') OR _secid_hash = public.digest(?, \'sha256\')',
-                $search,
-                $search,
-                $search,
-            ];
-            if (ctype_digit($search)) {
-                $filter[0] .= ' OR id = ? ';
-                $filter[] = $search;
-            }
-            $options = [
-                'order' => 'id',
-            ];
-            $subset = $user->paginate(Pagination::findCurrentPage() - 1, GK_PAGINATION_ADMIN_USER_SEARCH, $filter, $options);
-            Smarty::assign('users', $subset);
-            // Paginate
-            $pages = new Pagination($subset['total'], $subset['limit']);
-            Smarty::assign('pg', $pages);
+        $user = new User();
+        $filter = [
+            'lower(username) like lower(?) OR _email_hash = public.digest(lower(?), \'sha256\') OR _secid_hash = public.digest(?, \'sha256\')',
+            $search,
+            $search,
+            $search,
+        ];
+        if (ctype_digit($search)) {
+            $filter[0] .= ' OR id = ? ';
+            $filter[] = $search;
         }
+        $options = [
+            'order' => 'id DESC',
+        ];
+        $subset = $user->paginate(Pagination::findCurrentPage() - 1, GK_PAGINATION_ADMIN_USER_SEARCH, $filter, $options);
+        Smarty::assign('users', $subset);
+        // Paginate
+        $pages = new Pagination($subset['total'], $subset['limit']);
+        Smarty::assign('pg', $pages);
     }
 }
