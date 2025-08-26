608ac9debe8479fa039a6017d6c29eda1ae56788
Mathieu A <github@kumy.net>
2025-08-26 21:44:17 +0200

    fix: Add url to label links

--- website/app/GeoKrety/Service/Labels/Image.php
+++ website/app/GeoKrety/Service/Labels/Image.php
@@ -22,13 +22,14 @@ class Image {
     private function fetch(Geokret $geokret, string $type = 'svg') {
         Smarty::assign('geokret', $geokret);
 
+        $url = \Base::instance()->alias('move_create_short', '@tracking_code='.$geokret->tracking_code);
         // Localized Help
         // Both strings are necessary here as gettext cannot work with variable
         _('<tspan font-weight="bold">User\'s manual:</tspan> <tspan font-weight="bold">1.</tspan> Take this GeoKret. <tspan font-weight="bold">Please note down his Tracking Code.</tspan> <tspan font-weight="bold">2.</tspan> Hide in another cache. <tspan font-weight="bold">3.</tspan> Register the trip at <tspan font-weight="bold">https://geokrety.org</tspan>');
         $help = '<tspan font-weight="bold">User\'s manual:</tspan> <tspan font-weight="bold">1.</tspan> Take this GeoKret. <tspan font-weight="bold">Please note down his Tracking Code.</tspan> <tspan font-weight="bold">2.</tspan> Hide in another cache. <tspan font-weight="bold">3.</tspan> Register the trip at <tspan font-weight="bold">https://geokrety.org</tspan>';
         $translatedHelp = LanguageService::translate($help, array_diff($this->getLanguages(), ['en']));
         foreach ($translatedHelp as $key => $help) {
-            Smarty::assign('help'.$key, $help);
+            Smarty::assign('help'.$key, $help.$url);
         }
 
         $template = sprintf('labels/%s.tpl.svg', is_null($geokret->label_template) ? 'default' : $geokret->label_template->template);
