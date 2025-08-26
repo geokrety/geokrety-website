574e5e317823cfef71a3404eda4989ac07fd2133
Mathieu A <github@kumy.net>
2025-08-26 18:46:40 +0200

    ci: Try ci fix

--- website/db/tests-srtm-import.sh
+++ website/db/tests-srtm-import.sh
@@ -15,12 +15,13 @@ cleanup() {
 mkdir "$SRTM_DIR" || true
 
 echo "Getting SRTM Files…"
-wget -4 -c -i "$DIR/tests-srtm-import.txt" -P "$SRTM_DIR"
+wget -4 -xnH -c -i "$DIR/tests-srtm-import.txt" -P "$SRTM_DIR"
 
 echo "Generating SQL import file"
 # Ensure the postgis function are found from the public schema
 echo "SET search_path = public;" > "$QUERIES_FILE"
-raster2pgsql "$SRTM_DIR"/*.zip -F -I -e public.srtm >> "$QUERIES_FILE"
+echo "TRUNCATE public.srtm;" >> "$QUERIES_FILE"
+raster2pgsql "$SRTM_DIR"/*/*.zip -F -I -e public.srtm >> "$QUERIES_FILE"
 
 echo "Importing SQL import file…"
 psql -h 127.0.0.1 -U "${PGUSER:-geokrety}" "${DBNAME:-geokrety}" -f "$QUERIES_FILE"
