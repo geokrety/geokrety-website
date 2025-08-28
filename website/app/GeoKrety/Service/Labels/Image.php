0f472243434043582335d54c55432e7c2efc1abf
Mathieu A <github@kumy.net>
2025-08-28 18:49:10 +0200

    fix: Drop formating from strings translations

--- website/app/GeoKrety/Service/Labels/Image.php
+++ website/app/GeoKrety/Service/Labels/Image.php
@@ -22,21 +22,65 @@ class Image {
     private function fetch(Geokret $geokret, string $type = 'svg') {
         Smarty::assign('geokret', $geokret);
 
-        // Localized Help for next commit
+        $url = \Base::instance()->alias('move_create_short', '@tracking_code='.$geokret->tracking_code);
+        $languages = array_diff($this->getLanguages(), ['en']);
+
+        function translate(string $msg, array $languages, array $placeholders = []): array {
+            $strings = LanguageService::translate($msg, $languages);
+
+            return array_map(fn ($s) => vsprintf($s, $placeholders), $strings);
+        }
+
+        function bold(string|array $msg): string|array {
+            if (is_array($msg)) {
+                return array_map(
+                    fn ($m) => bold($m), // recursive call
+                    $msg
+                );
+            }
+
+            return "<tspan font-weight=\"bold\">{$msg}</tspan>";
+        }
+
+        function concatByLang(array $items): array {
+            $out = [];
+            foreach ($items as $item) {
+                if (is_array($item)) {
+                    foreach ($item as $lang => $text) {
+                        $out[$lang] = ($out[$lang] ?? '').$text;
+                    }
+
+                    continue;
+                }
+                foreach (array_keys($out) as $lang) {
+                    $out[$lang] .= $item;
+                }
+            }
+
+            return $out;
+        }
+        // This will be used to detect strings to translate
         _('User\'s manual:');
         _('Take this GeoKret.');
         _('Please note down his Tracking Code.');
         _('Hide in another cache.');
         _('Register the trip at %s');
 
-        $url = \Base::instance()->alias('move_create_short', '@tracking_code='.$geokret->tracking_code);
-        // Localized Help
-        // Both strings are necessary here as gettext cannot work with variable
-        _('<tspan font-weight="bold">User\'s manual:</tspan> <tspan font-weight="bold">1.</tspan> Take this GeoKret. <tspan font-weight="bold">Please note down his Tracking Code.</tspan> <tspan font-weight="bold">2.</tspan> Hide in another cache. <tspan font-weight="bold">3.</tspan> Register the trip at <tspan font-weight="bold">https://geokrety.org</tspan>');
-        $help = '<tspan font-weight="bold">User\'s manual:</tspan> <tspan font-weight="bold">1.</tspan> Take this GeoKret. <tspan font-weight="bold">Please note down his Tracking Code.</tspan> <tspan font-weight="bold">2.</tspan> Hide in another cache. <tspan font-weight="bold">3.</tspan> Register the trip at <tspan font-weight="bold">https://geokrety.org</tspan>';
-        $translatedHelp = LanguageService::translate($help, array_diff($this->getLanguages(), ['en']));
+        // Localized Help, use raw strings so they get translated
+        $strings = [];
+        $strings[] = bold(translate('User\'s manual:', $languages));
+        $strings[] = bold(' 1. ');
+        $strings[] = translate('Take this GeoKret.', $languages);
+        $strings[] = bold(' 2. ');
+        $strings[] = translate('Please note down his Tracking Code.', $languages);
+        $strings[] = bold(' 3. ');
+        $strings[] = translate('Hide in another cache.', $languages);
+        $strings[] = bold(' 4. ');
+        $strings[] = translate('Register the trip at %s', $languages, [GK_SITE_BASE_SERVER_URL.$url]);
+        $translatedHelp = concatByLang($strings);
+
         foreach ($translatedHelp as $key => $help) {
-            Smarty::assign('help'.$key, $help.$url);
+            Smarty::assign('help'.$key, $help);
         }
 
         $template = sprintf('labels/%s.tpl.svg', is_null($geokret->label_template) ? 'default' : $geokret->label_template->template);
