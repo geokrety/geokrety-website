2ba7cdc1cab5df9b8c154757cbaa04ce0e603e45
Mathieu A <github@kumy.net>
2025-09-06 19:14:27 +0200

    refactor: clean code

--- website/app/GeoKrety/Service/WaypointInfo.php
+++ website/app/GeoKrety/Service/WaypointInfo.php
@@ -11,31 +11,31 @@ class WaypointInfo {
     public const PREFIX_OTHER_1 = ['N']; // Navicache
     public const PREFIX_OTHER_3 = ['WPG'];
 
-    public static function isOC($waypoint) {
+    public static function isOC(string $waypoint): bool {
         return in_array(substr(strtoupper($waypoint), 0, 2), self::PREFIX_OC);
     }
 
-    public static function isGC($waypoint) {
+    public static function isGC(string $waypoint): bool {
         return in_array(substr(strtoupper($waypoint), 0, 2), self::PREFIX_GC);
     }
 
-    public static function isWPG($waypoint) {
+    public static function isWPG(string $waypoint): bool {
         return in_array(substr(strtoupper($waypoint), 0, 3), self::PREFIX_OTHER_3);
     }
 
-    public static function isNavicache($waypoint) {
-        return in_array(substr(strtoupper($waypoint), 0, 3), self::PREFIX_OTHER_1);
+    public static function isNavicache(string $waypoint): bool {
+        return in_array(substr(strtoupper($waypoint), 0, 1), self::PREFIX_OTHER_1);
     }
 
-    public static function isImportedWaypoint($waypoint) {
+    public static function isImportedWaypoint(string $waypoint): bool {
         return self::isOC($waypoint) or self::isWPG($waypoint) or self::isNavicache($waypoint);
     }
 
-    public static function getLink($waypoint) {
+    public static function getLink(string $waypoint): string {
         return sprintf(GK_SERVICE_GO2GEO_URL, $waypoint);
     }
 
-    public static function getLinkPosition($lat, $lon) {
+    public static function getLinkPosition($lat, $lon): string {
         return sprintf(GK_SERVICE_GC_SEARCH_NEAREST_URL, $lat, $lon);
     }
 }
