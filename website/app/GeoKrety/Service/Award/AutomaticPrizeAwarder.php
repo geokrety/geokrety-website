34bda0922ffb966ec2905a661718446d3b3e4f20
Mathieu A <github@kumy.net>
2025-09-29 20:10:56 +0200

    refactor(awards): centralize award logic in AutomaticPrizeAwarder

--- website/app/GeoKrety/Service/Award/AutomaticPrizeAwarder.php
+++ website/app/GeoKrety/Service/Award/AutomaticPrizeAwarder.php
@@ -2,6 +2,8 @@
 
 namespace GeoKrety\Service\Award;
 
+use GeoKrety\Model\Awards;
+use GeoKrety\Model\AwardsWon;
 use GeoKrety\Model\Move;
 
 class AutomaticPrizeAwarder {
@@ -36,7 +38,7 @@ class AutomaticPrizeAwarder {
 
                 if ($service instanceof AwardServiceInterface) {
                     if ($service->shouldAward($move)) {
-                        $service->awardUser($move);
+                        $this->awardUser($move, $service);
                     }
                 }
             } catch (\Exception $e) {
@@ -45,5 +47,51 @@ class AutomaticPrizeAwarder {
                 error_log("Award service error for {$serviceClass}: ".$e->getMessage());
             }
         }
+    }
+
+    /**
+     * Award the user using the provided service.
+     */
+    private function awardUser(Move $move, AwardServiceInterface $service): void {
+        // Load award with cache (1 hour TTL)
+        $award = new Awards();
+        $award->load(['name = ?', $service->getAwardName()], ttl: 3600);
+
+        if ($award->dry()) {
+            // Award doesn't exist - log and return
+            error_log(sprintf("Award '%s' not found in database", $service->getAwardName()));
+
+            return;
+        }
+
+        // Check if already awarded (with cache)
+        $awardWon = new AwardsWon();
+        $awardWon->load(['holder = ? and award = ?', $move->author->id, $award->id], ttl: 3600);
+
+        if (!$awardWon->dry()) {
+            return; // Already awarded
+        }
+
+        // Create and save award
+        $awardWon = new AwardsWon();
+        $awardWon->holder = $move->author->id;
+        $awardWon->award = $award->id;
+        $awardWon->description = $service->getAwardDescription($move);
+        $awardWon->save();
+
+        // Invalidate cache immediately
+        $invalidateCheck = new AwardsWon();
+        $invalidateCheck->load(['holder = ? and award = ?', $move->author->id, $award->id], ttl: -1);
+
+        // Fire event for audit trail
+        $events = \Sugar\Event::instance();
+        $events->emit('award.given', [
+            'award_id' => $award->id,
+            'award_name' => $service->getAwardName(),
+            'user_id' => $move->author->id,
+            'move_id' => $move->id,
+            'geokret_id' => $move->geokret->id,
+            'automatic' => true,
+        ]);
     }
 }
--- website/db/tests/test-300-awards.sql
+++ website/db/tests/test-300-awards.sql
@@ -9,26 +9,28 @@ INSERT INTO "gk_users" ("id", "username", "registration_ip") VALUES (2, 'test 2'
 INSERT INTO "gk_users" ("id", "username", "registration_ip") VALUES (3, 'test 3', '127.0.0.1');
 
 -- Test awards
-INSERT INTO "gk_awards" ("id", "name", "description", "filename", "type") VALUES (1, 'Test Award', 'Test award description', 'test.svg', 'manual');
-INSERT INTO "gk_awards" ("id", "name", "description", "filename", "type") VALUES (2, 'Hidden GeoKrety Finder', 'Has discovered one Hidden GeoKrety', 'hidden-finder.svg', 'manual');
-INSERT INTO "gk_awards" ("id", "name", "description", "filename", "type") VALUES (3, 'Another Award', 'Another test award', 'another.svg', 'manual');
+-- Starting high as the db dumps already contains awards;
+INSERT INTO "gk_awards" ("id", "name", "description", "filename", "type") VALUES (1000001, 'Test Award', 'Test award description', 'test.svg', 'manual');
+INSERT INTO "gk_awards" ("id", "name", "description", "filename", "type") VALUES (1000002, 'Hidden GeoKrety Finder', 'Has discovered one Hidden GeoKrety', 'hidden-finder.svg', 'manual');
+INSERT INTO "gk_awards" ("id", "name", "description", "filename", "type") VALUES (1000003, 'Another Award', 'Another test award', 'another.svg', 'manual');
 
 -- Test basic award assignment works
-SELECT lives_ok($$INSERT INTO "gk_awards_won" ("holder", "award", "description") VALUES (1, 1, 'Test award assignment')$$, 'Basic award assignment should work');
 
 -- Test unique constraint (holder, award) - user cannot get same award twice
+SELECT lives_ok($$INSERT INTO "gk_awards_won" ("holder", "award", "description") VALUES (1, 1000001, 'Test award assignment')$$, 'Basic award assignment should work');
+
 SELECT throws_ok(
-    $$INSERT INTO "gk_awards_won" ("holder", "award", "description") VALUES (1, 1, 'Duplicate award')$$,
+    $$INSERT INTO "gk_awards_won" ("holder", "award", "description") VALUES (1, 1000001, 'Duplicate award')$$,
     23505,
     'duplicate key value violates unique constraint "gk_awards_won_holder_award"',
     'Should prevent duplicate awards to same user'
 );
 
 -- Test same award can be given to different users
-SELECT lives_ok($$INSERT INTO "gk_awards_won" ("holder", "award", "description") VALUES (2, 1, 'Same award to different user')$$, 'Same award can be given to different users');
+SELECT lives_ok($$INSERT INTO "gk_awards_won" ("holder", "award", "description") VALUES (2, 1000001, 'Same award to different user')$$, 'Same award can be given to different users');
 
 -- Test different awards can be given to same user
-SELECT lives_ok($$INSERT INTO "gk_awards_won" ("holder", "award", "description") VALUES (1, 3, 'Different award to same user')$$, 'Different awards can be given to same user');
+SELECT lives_ok($$INSERT INTO "gk_awards_won" ("holder", "award", "description") VALUES (1, 1000003, 'Different award to same user')$$, 'Different awards can be given to same user');
 
 -- Test Easter Egg GeoKrety setup (type 10)
 INSERT INTO "gk_geokrety" ("id", "name", "type", "owner", "holder", "created_on_datetime") VALUES (1, 'Easter Egg Test', 10, 1, 1, '2024-07-21 12:15:00+00');
@@ -37,13 +39,13 @@ INSERT INTO "gk_geokrety" ("id", "name", "type", "owner", "holder", "created_on_
 INSERT INTO "gk_geokrety" ("id", "name", "type", "owner", "holder", "created_on_datetime") VALUES (4, 'Another Easter Egg', 10, 2, 2, '2024-07-21 12:15:00+00');
 
 -- Test award assignment can happen to different users for Hidden GeoKrety Finder
-SELECT lives_ok($$INSERT INTO "gk_awards_won" ("holder", "award", "description") VALUES (1, 2, 'Found Easter Egg')$$, 'User 1 can get Hidden GeoKrety Finder award');
-SELECT lives_ok($$INSERT INTO "gk_awards_won" ("holder", "award", "description") VALUES (2, 2, 'Found Easter Egg')$$, 'User 2 can get Hidden GeoKrety Finder award');
-SELECT lives_ok($$INSERT INTO "gk_awards_won" ("holder", "award", "description") VALUES (3, 2, 'Found Easter Egg')$$, 'User 3 can get Hidden GeoKrety Finder award');
+SELECT lives_ok($$INSERT INTO "gk_awards_won" ("holder", "award", "description") VALUES (1, 1000002, 'Found Easter Egg')$$, 'User 1 can get Hidden GeoKrety Finder award');
+SELECT lives_ok($$INSERT INTO "gk_awards_won" ("holder", "award", "description") VALUES (2, 1000002, 'Found Easter Egg')$$, 'User 2 can get Hidden GeoKrety Finder award');
+SELECT lives_ok($$INSERT INTO "gk_awards_won" ("holder", "award", "description") VALUES (3, 1000002, 'Found Easter Egg')$$, 'User 3 can get Hidden GeoKrety Finder award');
 
 -- Test same user cannot get Hidden GeoKrety Finder award twice
 SELECT throws_ok(
-    $$INSERT INTO "gk_awards_won" ("holder", "award", "description") VALUES (1, 2, 'Duplicate Easter Egg award')$$,
+    $$INSERT INTO "gk_awards_won" ("holder", "award", "description") VALUES (1, 1000002, 'Duplicate Easter Egg award')$$,
     23505,
     'duplicate key value violates unique constraint "gk_awards_won_holder_award"',
     'Should prevent duplicate Hidden GeoKrety Finder awards to same user'
